438f5ed1a60516adef8f226052f113fe6a4bf892
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-11 09:53:53 -0500

    Objectify (#6)
    * Converted to object
    * added Coveralls

--- tests/test_vbaProjectCompiler.py
+++ tests/test_vbaProjectCompiler.py
@@ -3,10 +3,18 @@
 import pytest, binascii
 from vbaProjectCompiler.main import *
 
+def test_constructor():
+    expected = "./Fo0"
+    project = VbaProject(expected)
+    result = project.path
+    assert result == expected
+
 def test_getFirstDirectoryChainSector():
-    assert getFirstDirectoryChainSector() == 1;
+    project = VbaProject('.')
+    assert project.getFirstDirectoryChainSector() == 1;
 
 def test_header():
-    result = writeHeader()
+    project = VbaProject('.')
+    result = project.header()
     expected = b'\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x3E\x00\x03\x00\xFE\xFF\x09\x00\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x10\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\xFE\xFF\xFF\xFF\x00\x00\x00\x00'
     assert result == expected
