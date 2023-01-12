0f0b293908aa592c75b4d3594011b0e45ba0b796
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-12 15:58:42 -0500

    Update test_vbaProjectCompiler.py

--- tests/test_vbaProjectCompiler.py
+++ tests/test_vbaProjectCompiler.py
@@ -56,4 +56,4 @@ def test_fileHandling():
     #thisWorkbook.addData(ThisWorkbookData)
     #vbaProject.addFile(thisWorkbook)
     import sys
-    sys.stderr.write(dir(ThisWorkbookData))
+    sys.stderr.write(ThisWorkbookData.decode('utf-8'))
