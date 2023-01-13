65db8cf631005eb16864374b101ceacf19df78b6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-13 10:06:42 -0500

    Update project.py

--- vbaProjectCompiler/project.py
+++ vbaProjectCompiler/project.py
@@ -15,7 +15,7 @@ class Project:
         # Use \x0D0A line endings...however python encodes that.
         string = '"VBAProject"\r\n'
         for att in self.attributes:
-            string += att.name + '="' + att.value . '"\r\n'
+            string += att.name + '="' + att.value + '"\r\n'
         string += '"\r\n\r\n'
         string += '[HostExtender Info]\r\n'
         string += self.hostExtenderInfo
