f1e34ead5acce8c2c9b99ea8248770b02bb111c5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-13 08:06:16 -0500

    Update test_fatSectors.py

--- tests/test_fatSectors.py
+++ tests/test_fatSectors.py
@@ -14,3 +14,13 @@ def test_defaults():
     #Test that when no data is present, all streams are terminated after the first sector
     assert project.fatChain == [-2, -2, -2]
     
+def test_longerDirectoryList():
+    #Test that when we add four files to the directory, the directory list is one longer.
+    project = VbaProject()
+    blankFile = Directory()
+    project.AddFile(blankFile)
+    project.AddFile(blankFile)
+    project.AddFile(blankFile)
+    project.AddFile(blankFile)
+    assert project.countDirectoryListSectors() == 2
+    assert project.fatChain == [4, -2, -2, -2]
