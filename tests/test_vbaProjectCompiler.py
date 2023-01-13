c72d75beb80f9e0caee4e48b6d8e0e2051a08c4f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-13 11:01:14 -0500

    Update test_vbaProjectCompiler.py

--- tests/test_vbaProjectCompiler.py
+++ tests/test_vbaProjectCompiler.py
@@ -3,20 +3,14 @@
 import pytest, binascii, sys
 from vbaProjectCompiler.main import *
 
-def test_constructor():
-    expected = "./Fo0"
-    project = VbaProject(expected)
-    result = project.path
-    assert result == expected
-
 def test_getFirstDirectoryListSector():
-    project = VbaProject('.')
+    project = VbaProject()
     assert project.getFirstDirectoryListSector() == 1
     project.setFirstDirectoryListSector(2)
     assert project.getFirstDirectoryListSector() == 2
 
 def test_header():
-    project = VbaProject('.')
+    project = VbaProject()
     result = project.header()
     expected = b'\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x3E\x00\x03\x00\xFE\xFF\x09\x00\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x10\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\xFE\xFF\xFF\xFF\x00\x00\x00\x00'
     empty = b'\xff\xff\xff\xff'
