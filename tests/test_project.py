3c71169b6f70c856d499528b74ee047caf8eaee1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-13 17:13:26 -0500

    Update test_project.py

--- tests/test_project.py
+++ tests/test_project.py
@@ -20,7 +20,7 @@ def test_blank():
     
     #expected = Path("tests/blank/vbaProject.bin").read_text()
     file = open("tests/blank/vbaProject.bin", "rb")
-    file.seek(int('0x2500', 0))
+    file.seek(int('0x2400', 0))
     expected = file.read(int('0x152', 0))
 
     result = project.toBytearray()
