2283a7856c337cee17994644c5b626a26b00c13b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-13 14:59:14 -0500

    Update test_project.py

--- tests/test_project.py
+++ tests/test_project.py
@@ -18,6 +18,9 @@ def test_blank():
     project.addWorkspace("Sheet1", 0, 0, 0, 0, 'C')
     project.addWorkspace("Module1", 26, 26, 1349, 522, 'Z')
     
-    expected = Path("tests/blank/PROJECT").read_text()
-    result = project.toString()
+    #expected = Path("tests/blank/PROJECT").read_text()
+    file = open("tests/blank/PROJECT", "rb")
+    expected = file.read()
+
+    result = butearray(project.toString(), "ascii")
     assert expected == result
