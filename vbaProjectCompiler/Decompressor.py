1f4f8b8dfcb7b77ef8337d29a33baf7b4eedc30b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-13 22:53:43 -0500

    Update Decompressor.py

--- vbaProjectCompiler/Decompressor.py
+++ vbaProjectCompiler/Decompressor.py
@@ -1,4 +1,6 @@
 class Decompressor:
     def calculateChunkSize(compressedHeader):
         """Given the first 2 bytes of a compressed chunk, return the chunk size in bytes"""
-         
+        if int(compressedHeader) % 2 == 1:
+            return 4095
+        return compressedHeader >> 4 + 3
