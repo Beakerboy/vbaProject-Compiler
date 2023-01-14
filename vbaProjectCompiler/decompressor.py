680d1c05122a7281fdf5aed4428d82d56f7c14c5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-14 14:21:06 -0500

    Update decompressor.py

--- vbaProjectCompiler/decompressor.py
+++ vbaProjectCompiler/decompressor.py
@@ -48,5 +48,5 @@ class Decompressor:
     def getCompressedChunk(self):
         compressedChunkFlag = 1 if self.compressed else 0
         intHeader = (self.compressed << 15) | 0x7000 | (self.compressedChunkSize - 3)
-        compressedChunkHeader = struct.pack("<h", intHeader)
+        compressedChunkHeader = struct.pack("<H", intHeader)
         return compressedChunkHeader + self.compressedData
