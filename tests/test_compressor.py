755257025ec836707b8d804edcd029850342eece
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-14 14:57:32 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -11,3 +11,11 @@ def test_Compressor():
     data = b'\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E'
     comp.setCompressedData(data)
     assert comp.getCompressedChunk() == bytearray(header) + bytearray(data)
+
+def test_unableToCompress():
+    comp = Decompressor()
+    comp.setCompression(True)
+    input = "abcdefghijklmnopqrstuv."
+    #result = comp.compress(input);
+    #expected = b'\x01\x19\xB0\x00\x61\c62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E'
+    #assert expected == result
