e383b267bd33197cd6fad43d8ea330d40329e791
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-14 13:20:14 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -3,7 +3,9 @@ from vbaProjectCompiler.decompressor import Decompressor
 
 def test_Compressor():
     comp = Decompressor()
-    comp.setCompressedHeader(b'\x22\xB0')
-    expected = 37
+    comp.setCompressedHeader(b'\x19\xB0')
+    expected = 28
     result = comp.getCompressedChunkSize()
     assert expected == result
+    data = b'\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E'
+    comp.setCompressedData(data)
