95d5c2d3ab17f2352138a4444b9018756a72abb4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-14 08:11:19 -0500

    Create test_compressor.py

---
+++ tests/test_compressor.py
@@ -0,0 +1,9 @@
+import pytest
+from vbaProjectCompiler.decompressor import Decompressor
+
+def test_Compressor():
+    comp = Decompressor()
+    comp.setCompressedHeader(b'0x1603')
+    expected = 355
+    result = comp.getChunkSize
+    assert expected = result
