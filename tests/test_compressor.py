01cbe47ac883c9c8fc490dee2548636c07472375
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-14 13:33:37 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -10,4 +10,4 @@ def test_Compressor():
     assert expected == result
     data = b'\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E'
     comp.setCompressedData(data)
-    assert comp.getCompressedChunk() == bytearray(header) + data
+    assert comp.getCompressedChunk() == bytearray(header) + bytearray(data)
