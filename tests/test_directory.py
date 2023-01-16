62d61b35b113fec6decc4409cb77c664322946be
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-16 18:08:06 -0500

    Update test_directory.py

--- tests/test_directory.py
+++ tests/test_directory.py
@@ -29,21 +29,6 @@ def test_RootDirectory():
     dir.addFile(stream)
     assert dir.fileSize() == 384
 
-def test_StorageDirectory():
-    #why is this not cleaned up? Need to used dir1 for some reason
-    dir1 = StorageDirectory()
-    assert dir1.type == 1
-    assert len(dir1.directories) == 0
-    assert dir1.minifatSectorsUsed() == 0
-
-def test_addFile():
-    dir2 = StorageDirectory()
-    stream = StreamDirectory()
-    stream.filePath = "tests/blank/PROJECT"
-    dir2.addFile(stream)
-    assert dir2.minifatSectorsUsed() == 6
-    assert dir2.fileSize() == 0
-
 def test_StreamDirectory():
     dir = StreamDirectory()
     assert dir.type == 2
