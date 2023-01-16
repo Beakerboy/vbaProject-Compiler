2c626fba22b665e21f0377d771e6867de594f183
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-16 11:31:34 -0500

    Update storageDirectory.py

--- vbaProjectCompiler/Directories/storageDirectory.py
+++ vbaProjectCompiler/Directories/storageDirectory.py
@@ -3,5 +3,14 @@ from vbaProjectCompiler.Directories.directory import Directory
 class StorageDirectory(Directory):
     type = 1
     directories = []
+
     def fileSize(self):
         return 0
+
+    def minifatSectorsUsed(self):
+        #Nesd to use the value from the header
+        minifatSectorSize = 64
+        size = 0
+        for dir in directories:
+            size += minifatSectorsUsed()
+        return size
