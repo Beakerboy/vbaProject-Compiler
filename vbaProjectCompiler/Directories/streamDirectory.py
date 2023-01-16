735c7104d7435ccd1ed1f8d21ea799b2d61742bd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-16 10:25:29 -0500

    Create streamDirectory.py

--- vbaProjectCompiler/Directories/streamDirectory.py
+++ vbaProjectCompiler/Directories/streamDirectory.py
@@ -3,3 +3,9 @@ from vbaProjectCompiler.Directories.directory import Directory
 class StreamDirectory(Directory):
     type = 2
     filePath = ""
+
+    def fileSize(self):
+        if self.filePath == "":
+            return 0
+        file_size = os.stat(self.filePath)
+        return file_size.st_size
