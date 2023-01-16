cf6fc42e1817325af90e2cd6b520a4c85686a551
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-16 12:10:56 -0500

    Update streamDirectory.py

--- vbaProjectCompiler/Directories/streamDirectory.py
+++ vbaProjectCompiler/Directories/streamDirectory.py
@@ -6,10 +6,13 @@ class StreamDirectory(Directory):
     filePath = ""
 
     def fileSize(self):
+        """
+        Size in bytes of the file after it has been compressed
+        """
         if self.filePath == "":
             return 0
         file_size = os.stat(self.filePath)
         return file_size.st_size
 
     def minifatSectorsUsed(self):
-        return (self.fileSize - 1) // 64 + 1
+        return (self.fileSize() - 1) // 64 + 1
