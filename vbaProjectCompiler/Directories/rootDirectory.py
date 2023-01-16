343fa1fa5e26e3b0f96da2d637462b3354ee1d6f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-16 10:30:55 -0500

    Update rootDirectory.py

--- vbaProjectCompiler/Directories/rootDirectory.py
+++ vbaProjectCompiler/Directories/rootDirectory.py
@@ -2,3 +2,10 @@ from vbaProjectCompiler.Directories.directory import Directory
 
 class RootDirectory(Directory):
     type = 5
+
+    directories = []
+
+    def fileSize(self):
+        #iterate through the directories list
+        #if the item is a stream, ask for its size and round
+        #up to fill the minifat block.
