b9caa1b3a2a0a60ab640183c37661760eab85612
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-18 06:16:00 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -30,9 +30,9 @@ def test_maxCompression():
 
 def test_normalCompression():
     comp = Decompressor()
-    comp.setCompression(True)
+    %comp.setCompression(True)
     input = "#aaabcdefaaaaghijaaaaaklaaamnopqaaaaaaaaaaaarstuvwxyzaaa"
-    result = comp.compress(input);
+    #result = comp.compress(input);
     expected = b'\x01\x2F\xB0\x00\x23\x61\x61\x61\x62\x63\x64\x65\x82\x66\x00\x70\x61\x67\x68\x69\x6A\x01\x38\x08\x61\x6B\x6C\x00\x30\x6D\x6E\x6F\x70\x06\x71\x02\x70\x04\x10\x72\x73\x74\x75\x76\x10\x77\x78\x79\x7A\x00\x3C' 
     #assert expected == result
 
