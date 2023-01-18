3add02a02cce8fd60d81770ff27733b70c44aa5e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-18 07:54:57 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -79,7 +79,7 @@ def test_ceilLog2Zero():
 def test_decompressUnableToCompress():
     compressed = bytearray(b'\x01\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E')
     comp = Decompressor()
-    header = compressed.pop() + compressed.pop()
+    header = bytearray(compressed.pop() + compressed.pop())
     comp.setCompressedHeader(header)
     result = comp.decompress(compressed)
     expected = "abcdefghijklmnopqrstuv."
