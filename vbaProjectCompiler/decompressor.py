0769f40860baa9372011e96c2597975bad93f8e7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-18 09:56:45 -0500

    Update decompressor.py

--- vbaProjectCompiler/decompressor.py
+++ vbaProjectCompiler/decompressor.py
@@ -113,7 +113,7 @@ class Decompressor:
         calculate an offset and length from a copytoken
         """
         help = self.copytokenHelp()
-        length = copyToken & help["lengthMask"] + 3
+        length = (copyToken & help["lengthMask"]) + 3
         temp1 = copyToken & help["offsetMask"]
         temp2 = 16 - help["bitCount"]
         offset = temp1 >> temp2 + 1
