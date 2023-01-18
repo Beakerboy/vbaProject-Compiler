ee8772f6b61db2549b972f61f883560c68cc3a98
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-18 08:28:46 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -77,11 +77,12 @@ def test_ceilLog2Zero():
         comp.ceilLog2(0)
 
 def test_decompressUnableToCompress():
-    compressed = bytearray(b'\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E')
+    #compressed = bytearray(b'\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E')
+    compressed = bytearray(b'\x08\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68')
     comp = Decompressor()
     header = bytearray(compressed[:2])
     del compressed[:2]
     comp.setCompressedHeader(header)
     result = comp.decompress(compressed)
-    expected = "abcdefghijklmnopqrstuv."
+    expected = "abcdefgh"
     assert expected == result
