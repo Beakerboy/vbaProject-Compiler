a5784860083815e5f71981507ac21367bdccac94
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-18 09:41:56 -0500

    Update decompressor.py (#21)
    * decompress strings without copy tokens

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -11,6 +11,40 @@ def test_Compressor():
     data = b'\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E'
     comp.setCompressedData(data)
     assert comp.getCompressedChunk() == bytearray(header) + bytearray(data)
+    
+def test_unableToCompress():
+    comp = Decompressor()
+    #comp.setCompression(True)
+    input = "abcdefghijklmnopqrstuv."
+    #result = comp.compress(input);
+    expected = b'\x01\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E'
+    #assert expected == result
+
+def test_maxCompression():
+    comp = Decompressor()
+    #comp.setCompression(True)
+    #input = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    #result = comp.compress(input);
+    expected = "\x01\x03\xB0\x02\x61\x45\x00"
+    #assert expected == result
+
+def test_normalCompression():
+    comp = Decompressor()
+    #comp.setCompression(True)
+    input = "#aaabcdefaaaaghijaaaaaklaaamnopqaaaaaaaaaaaarstuvwxyzaaa"
+    #result = comp.compress(input);
+    expected = b'\x01\x2F\xB0\x00\x23\x61\x61\x61\x62\x63\x64\x65\x82\x66\x00\x70\x61\x67\x68\x69\x6A\x01\x38\x08\x61\x6B\x6C\x00\x30\x6D\x6E\x6F\x70\x06\x71\x02\x70\x04\x10\x72\x73\x74\x75\x76\x10\x77\x78\x79\x7A\x00\x3C' 
+    #assert expected == result
+
+def test_compressRaw():
+    comp = Decompressor()
+    #comp.setCompression(False)
+    #input = "a"
+    #result = comp.compress(input);
+    #header = bytearray(b'\xFF\x3F')
+    #start = header + b'\x61'
+    #expected = start.ljust(4095, b'\x00')
+    #assert expected == result
 
 def test_ChunkSizeMismatch():
     comp = Decompressor()
@@ -19,3 +53,63 @@ def test_ChunkSizeMismatch():
     data = b'\x00\x61\x62'
     with pytest.raises(Exception) as e_info:
         comp.setCompressedData(data)
+
+def test_decompression():
+    chunk = b'\xA8\xB0\x00\x41\x74\x74\x72\x69\x62\x75\x74\x00\x65\x20\x56\x42\x5F\x4E\x61\x6D\x00\x65\x20\x3D\x20\x22\x53\x68\x65\x40\x65\x74\x31\x22\x0D\x0A\x0A\xE8\x42\x04\x61\x73\x02\x74\x30\x7B\x30\x30\x30\xC0\x32\x30\x38\x32\x30\x2D\x00\x20\x04\x08\x0E\x43\x00\x14\x02\x1C\x01\x24\x30\x30\x34\x36\x02\x7D\x0D\x7C\x47\x6C\x6F\x62\x61\x6C\x21\x01\xC4\x53\x70\x61\x63\x01\x92\x46\x61\x08\x6C\x73\x65\x0C\x64\x43\x72\x65\x61\x10\x74\x61\x62\x6C\x15\x1F\x50\x72\x65\x20\x64\x65\x63\x6C\x61\x00\x06\x49\x64\x11\x00\xAB\x54\x72\x75\x0D\x42\x45\x78\x70\x08\x6F\x73\x65\x14\x1C\x54\x65\x6D\x70\x00\x6C\x61\x74\x65\x44\x65\x72\x69\x06\x76\x02\x24\x92\x42\x75\x73\x74\x6F\x6D\x0C\x69\x7A\x04\x44\x03\x32'
+    comp = Decompressor()
+    #comp.setCompressedHeader(chunk[:2])
+    #assert comp.compressedChunkSize == 168
+    #assert len(chunk) == 168
+    #expected = 'Attribute VB_Name = "Sheet1"\x0D\x0A'
+    
+def test_cielLog2():
+    comp = Decompressor()
+    assert comp.ceilLog2(1) == 0
+    assert comp.ceilLog2(2) == 1
+    assert comp.ceilLog2(3) == 2
+    assert comp.ceilLog2(4) == 2
+    assert comp.ceilLog2(9) == 4
+    assert comp.ceilLog2(50) == 6
+
+def test_ceilLog2Zero():
+    comp = Decompressor()
+    with pytest.raises(Exception) as e_info:
+        comp.ceilLog2(0)
+
+def test_decompressUnableToCompressOneToken():
+    #compressed = bytearray(b'\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E')
+    compressed = bytearray(b'\x08\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68')
+    comp = Decompressor()
+    header = bytearray(compressed[:2])
+    del compressed[:2]
+    comp.setCompressedHeader(header)
+    result = comp.decompress(compressed)
+    expected = "abcdefgh"
+    assert expected == result
+
+def test_zeroTokens():
+    compressed = bytearray(b'\x00\xB0\x00')
+    comp = Decompressor()
+    header = bytearray(compressed[:2])
+    del compressed[:2]
+    comp.setCompressedHeader(header)
+    with pytest.raises(Exception) as e_info:
+        result = comp.decompress(compressed)
+    
+def test_decompressUnableToCompressOneToken():
+    compressed = bytearray(b'\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E')
+    comp = Decompressor()
+    header = bytearray(compressed[:2])
+    del compressed[:2]
+    comp.setCompressedHeader(header)
+    result = comp.decompress(compressed)
+    expected = "abcdefghijklmnopqrstuv."
+    assert expected == result
+
+def test_CopytokenHelp():
+    comp = Decompressor()
+    comp.uncompressedData = 'Attribute VB_Name = "Sheet1"\x0D\x0A'
+    result = comp.copytokenHelp()
+    assert result["bitCount"] == 5
+    assert result["lengthMask"] == 0x07FF
+    assert result["offsetMask"] == 0xF800
--- vbaProjectCompiler/decompressor.py
+++ vbaProjectCompiler/decompressor.py
@@ -1,19 +1,21 @@
 import struct
 class Decompressor:
     #class attributes
+
+    endien                   = ''
     #is the data compressed?
     compressed               = 1
 
     #the size in bytes of the chunk after compression
     compressedChunkSize      = 0
 
-    #the chunk signature must be 3
-    compressedChunkSignature = 3
-
     #The chunk after compression
     compressedData           = b''
 
     uncompressedData         = ""
+
+    def __init__(self, endien = 'little'):
+        self.endien = endien
 
     def setCompressedData(self, data):
         """set the Compressed data attribute"""
@@ -23,7 +25,7 @@ class Decompressor:
 
     def setCompression(self, compress):
         """Set if we want to compress the source or include it raw"""
-        self.compressed = compress
+        self.compressed = 1 if compress else 0
 
     def setCompressedHeader(self, compressedHeader):
         """The compressed header is two bytes. 12 signature byes followed by \011 and a single bit that is 0b1 if compressed"""
@@ -46,7 +48,92 @@ class Decompressor:
         return self.compressedChunkSize
 
     def getCompressedChunk(self):
+        
+        return self.getCompressedChunkHeader() + self.compressedData
+
+    def compress(self, input):
+        if len(input) > 4096:
+            raise Exception("Input cannot be longer than 4096 bytes.")
+        if self.compress:
+            self.compressStandard(input)
+        else:
+            self.compressRaw(input)
+        return self.getCompressedChunkHeader() + self.compressedData
+
+    def getCompressedChunkHeader(self):
         compressedChunkFlag = 1 if self.compressed else 0
         intHeader = (self.compressed << 15) | 0x3000 | (self.compressedChunkSize - 3)
-        compressedChunkHeader = struct.pack("<H", intHeader)
-        return compressedChunkHeader + self.compressedData
+        if intHeader > (2*0x7fff) or 0 > intHeader:
+            raise Exception('intHeader out of range: ' + str(intHeader))
+        return struct.pack("<H", intHeader)
+
+    def compressRaw(self, input):
+        self.compressedChunkSize = 4098
+        self.compressedData = input.ljust(4096, '\0')
+
+    def compressStandard(self, input):
+        pass
+
+    def decompress(self, data):
+        while len(data) > 0:
+          #flag is one byte
+          flagToken = data.pop(0)
+          if len(data) == 0:
+              raise Exception("There must be at least one token in each TokenSequence.")
+          flagMask = 1
+          for i in range(8):
+              flag = flagToken & flagMask
+              flagMask = flagMask << 1
+              if flag == 0:
+                  if len(data) > 0:
+                      self.uncompressedData += chr(data.pop(0))
+              else:
+                  if len(data) < 2:
+                      raise Exception("Copy Token does not exist. FlagToken was " + str(flagToken) + " and decompressed chunk is " + self.uncompressedData + '.')
+                  copyToken = struct.unpack("<H", data[:2])
+        return self.uncompressedData
+
+    def copytokenHelp(self):
+        """
+        Calculate a lengthMask, offsetMask, and bitCount
+        """
+        difference = len(self.uncompressedData)
+        bitCount = max(self.ceilLog2(difference), 4)
+        lengthMask = 0xFFFF >> bitCount
+        offsetMask = ~lengthMask & 0xFFFF
+        maxLength = 0xFFFF << bitCount + 3
+        return {
+            "lengthMask": lengthMask,
+            "offsetMask": offsetMask,
+            "bitCount": bitCount
+        }
+
+    def unpackCopytoken(self, copyToken):
+        """
+        calculate an offset and length from a copytoken
+        """
+        help = self.copytokenHelp()
+        length = copyToken & help.lengthMask + 3
+        temp1 = copyToken & help.offsetMask
+        temp2 = 16 - help.bitCount
+        offset = temp1 >> temp2 + 1
+        return {
+            "length": length,
+            "offset": offset
+        }
+
+    def ceilLog2(self, int):
+        """
+        calculate the log2 of the integer, rounded up to the nearest integer
+        """
+        orig_int = int
+        if int == 0:
+            raise Exception("zero not allowed")
+        i = 0
+        int = int >> 1
+        while int != 0:
+            i += 1
+            int = int >> 1
+        if 2**i < orig_int:
+            i += 1
+        return i
