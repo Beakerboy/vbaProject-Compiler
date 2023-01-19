34a0ee058bc7974b0704069d57f0f0b98905dc3d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-19 11:58:21 -0500

    Update dirStream.py

--- vbaProjectCompiler/Directories/dirStream.py
+++ vbaProjectCompiler/Directories/dirStream.py
@@ -6,11 +6,34 @@ class DirStream(StreamDirectory):
     """
 
     def __init__(self):
-        self.information = []
-        self.references = []
+        
+        syskind = {"id": 1, "size": 4, "value": 3] #0=16bit, 1=32bit, 2=mac, 3=64bit
+        compatVersion = [74, 4, 3]
+        lcid = [2, 4, 0x0409]
+        lcidInvoke = [12, 4, 0x0409]
+        codePage = [3, 2, 0x04E4]
+        projectName = [4, 10, "VBAProject"]
+        self.information = [
+            syskind,
+            compatVersion,
+            lcid,
+            lcidInvoke,
+            codePage,
+            projectName
+        ]
+        self.references  = []
         self.modules = []
 
     def toBytes(self):
-        #return
-        #information + references + modules + 0x0010 + 0x00000000
-        pass
+        output = b''
+        for record in self.information
+            format = "<HI"
+            if isinstance(record["value"], str):
+                format += record["size"] + "s"
+            elif record["size"] == 2:
+                format += "H"
+            el if record["size"] == 4:
+                format += "I"
+            output += struct.pack(format, record["id"], record["size"])
+            if size == 2:
+        return output
