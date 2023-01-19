86a03df101e089d8369860e0925bd4da3d9b5c12
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-19 08:52:03 -0500

    Delete test_fileIO.py

--- tests/test_fileIO.py
+++
@@ -1,9 +0,0 @@
-# test_fileIO.py
-from os.path import exists
-from vbaProjectCompiler.vbaProject import VbaProject
-
-def test_fileCreation():
-    project = VbaProject()
-    filePath = './vbaProject.bin'
-    project.writeFile(filePath)
-    assert exists(filePath)
