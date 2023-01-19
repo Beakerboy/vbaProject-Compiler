f016f3b917fc448e78891e6877c76ec12dc20930
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-19 11:23:57 -0500

    Update test_compressor.py (#35)
    * created a hex dump helper function
    * extracted dir stream to tests/blank

--- vbaProjectCompiler/main.py
+++ vbaProjectCompiler/main.py
@@ -20,3 +20,20 @@ def main(args):
     with open(rootPath + '/vbaProject.bin', 'w+') as file:
         file.write(vbaProject.header())
         #iterate along the FAT chain to write the bin
+
+def hexDumpString(data):
+    decompressed = bytearray(data, encoding="charmap")
+    count = 0
+    output = ''
+    while len(decompressed) > 0:
+        output += format(count, 'X').rjust(8, '0') + '   '
+        #get 16 Bytes
+        stringified = ''
+        for i in range(min(16, len(decompressed))):
+            char = decompressed.pop(0)
+            output += format(char, 'X').rjust(2, '0') + ' '
+            sChar = '.' if char < 32 else chr(char)
+            stringified += sChar
+        output += ' ' + stringified + '\n'
+        count += 16
+    return output
