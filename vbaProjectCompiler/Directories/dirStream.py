d18fe382031eef10748cc9cb50c7844b3c875e9d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-19 12:41:43 -0500

    Update dirStream.py (#36)
    * begin filling in data for the dir Stream

--- vbaProjectCompiler/Directories/dirStream.py
+++ vbaProjectCompiler/Directories/dirStream.py
@@ -1,3 +1,4 @@
+import struct
 from vbaProjectCompiler.Directories.streamDirectory import StreamDirectory
 
 class DirStream(StreamDirectory):
@@ -6,11 +7,48 @@ class DirStream(StreamDirectory):
     """
 
     def __init__(self):
-        self.information = []
-        self.references = []
+        syskind = SimpleRecord(1, 4, 3) #0=16bit, 1=32bit, 2=mac, 3=64bit
+        compatVersion = SimpleRecord(74, 4, 3)
+        lcid = SimpleRecord(2, 4, 0x0409)
+        lcidInvoke = SimpleRecord(20, 4, 0x0409)
+        codePage = SimpleRecord(3, 2, 0x04E4)
+        projectName = SimpleRecord(4, 10, "VBAProject")
+        self.information = [
+            syskind,
+            compatVersion,
+            lcid,
+            lcidInvoke,
+            codePage,
+            projectName
+        ]
+        self.references  = []
         self.modules = []
 
     def toBytes(self):
-        #return
-        #information + references + modules + 0x0010 + 0x00000000
-        pass
+        output = b''
+        for record in self.information:
+            output += record.pack()
+        return output
+
+class SimpleRecord():
+    def __init__(self, id, size, value):
+        self.id = id
+        self.size = size
+        self.value = value
+
+    def toDict(self):
+        return {"id": self.id, "size": self.size, "value": self.value}
+
+    def pack(self):
+        format = "<HI"
+        if isinstance(self.value, str):
+            self.stringValue = self.value
+            self.value = bytes(self.value, encoding = "ascii")
+            format += str(self.size) + "s"
+        elif self.size == 2:
+            format += "H"
+        elif self.size == 4:
+            format += "I"
+        output = struct.pack(format, self.id, self.size, self.value)
+        #clean up stringValue
+        return output
