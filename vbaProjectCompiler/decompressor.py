dce37e47d892192418143b11ff5633b4f609f8ec
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-20 10:46:48 -0500

    Update decompressor.py

--- vbaProjectCompiler/decompressor.py
+++ vbaProjectCompiler/decompressor.py
@@ -104,7 +104,7 @@ class Decompressor:
                       offset = copyToken["offset"]
                       length = len(self.uncompressedData)
                       if length < offset:
-                          raise Exception("copyToken offset: " + str(offset) + " while string length is " + str(length) + "\nOriginal data is: " + orig_data)
+                          raise Exception("copyToken offset: " + str(offset) + " while string length is " + str(length) + "\nOriginal data is: " + str(orig_data, "charmap"))
                       self.uncompressedData += bytes(self.uncompressedData[-1 * offset])
         return self.uncompressedData
 
