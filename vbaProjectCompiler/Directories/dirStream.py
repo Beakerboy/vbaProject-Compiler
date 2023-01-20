8e7904052cba0b1365832cbb2305c12ff2c5e436
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-20 08:57:07 -0500

    Update dirStream.py

--- vbaProjectCompiler/Directories/dirStream.py
+++ vbaProjectCompiler/Directories/dirStream.py
@@ -49,7 +49,8 @@ class DirStream(StreamDirectory):
        
         refName1 = SimpleRecord(0x0016, 6, refString.encode(codePageName)) #should be encoded using CodePage
         refName2 = SimpleRecord(0x003E, 12, refString.encode("utf_16_le"))
-        refRegistered = PackedRecord(struct.pack("<III94s",0x000D, 0x0068, 0x005E, "*\\G{00020430-0000-0000-C000-000000000046}#2.0#0#C:\\Windows\\System32\\stdole2.tlb#OLE Automation"))
+        text = "*\\G{00020430-0000-0000-C000-000000000046}#2.0#0#C:\\Windows\\System32\\stdole2.tlb#OLE Automation"
+        refRegistered = PackedRecord(struct.pack("<III94s",0x000D, 0x0068, 0x005E, text.encode(codePageName)))
        
         self.references  = [
             refName1,
