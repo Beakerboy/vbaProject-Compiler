e422ee9da2ca9eefc0df45fa67b1127b07ff66e3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-20 13:58:23 -0500

    Update dirStream.py

--- vbaProjectCompiler/Directories/dirStream.py
+++ vbaProjectCompiler/Directories/dirStream.py
@@ -164,6 +164,6 @@ class ReferenceRecord():
     def pack(self):
         strlen = len(self.libidRef.toString())
         format = "<HII" + str(strlen) + "sIH"
-        refRegistered = PackedRecord(struct.pack(format, 0x000D, 0x0068, 0x005E, self.libidRef.toString().encode(self.codePageName), 0, 0))
+        refRegistered = PackedRecord(struct.pack(format, 0x000D, strlen + 10, strlen, self.libidRef.toString().encode(self.codePageName), 0, 0))
        
         return self.RefName1.pack() + self.RefName2.pack() + refRegistered.pack()
