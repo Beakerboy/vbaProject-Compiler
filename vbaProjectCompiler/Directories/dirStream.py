875964b5b0ef75e9dc662b94716961d011b7a9d6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-20 09:06:48 -0500

    Update dirStream.py

--- vbaProjectCompiler/Directories/dirStream.py
+++ vbaProjectCompiler/Directories/dirStream.py
@@ -50,7 +50,7 @@ class DirStream(StreamDirectory):
         refName1 = SimpleRecord(0x0016, 6, refString.encode(codePageName))
         refName2 = SimpleRecord(0x003E, 12, refString.encode("utf_16_le"))
         text = "*\\G{00020430-0000-0000-C000-000000000046}#2.0#0#C:\\Windows\\System32\\stdole2.tlb#OLE Automation"
-        refRegistered = PackedRecord(struct.pack("<III94s",0x000D, 0x0068, 0x005E, text.encode(codePageName)))
+        refRegistered = PackedRecord(struct.pack("<III94sI",0x000D, 0x0068, 0x005E, text.encode(codePageName), 0))
        
         self.references  = [
             refName1,
