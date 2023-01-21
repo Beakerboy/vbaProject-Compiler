c9d533ccf01d922ed4d3fe4c882c0ca0af61c07b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-21 15:41:08 -0500

    Create packedData.py

---
+++ vbaProjectCompiler/Models/Fields/packedData.py
@@ -0,0 +1,10 @@
+class PackedData():
+    """
+    Mutivalue field with a packing format
+    """
+    def __init__(self, format, *values):
+        self.values  = values
+        self.format = format
+
+    def pack(self):
+        return struct.pack("<" + self.format, *self.values
