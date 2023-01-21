dcc62b9c59af29a7dad21943541782d57b6bba9b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-21 15:09:02 -0500

    Update dirStream.py

--- vbaProjectCompiler/Directories/dirStream.py
+++ vbaProjectCompiler/Directories/dirStream.py
@@ -11,17 +11,17 @@ class DirStream(StreamDirectory):
     def __init__(self):
         self.codePage = 0x04E4
         codePageName = "cp" + str(self.codePage)
-        syskind = SimpleRecord(1, 4, 3) #0=16bit, 1=32bit, 2=mac, 3=64bit
-        compatVersion = SimpleRecord(74, 4, 3)
-        lcid = SimpleRecord(2, 4, 0x0409)
-        lcidInvoke = SimpleRecord(20, 4, 0x0409)
-        codePageRecord = SimpleRecord(3, 2, self.codePage)
-        projectName = SimpleRecord(4, 10, "VBAProject")
+        syskind = IdSizeField(1, 4, 3) #0=16bit, 1=32bit, 2=mac, 3=64bit
+        compatVersion = IdSizeField(74, 4, 3)
+        lcid = IdSizeField(2, 4, 0x0409)
+        lcidInvoke = IdSizeField(20, 4, 0x0409)
+        codePageRecord = IdSizeField(3, 2, self.codePage)
+        projectName = IdSizeField(4, 10, "VBAProject")
         docString = DoubleEncodedSimple(codePageName, [5, 0x0040], "")
         helpfile = DoubleEncodedSimple(codePageName, [6, 0x003D], "")
-        helpContext = SimpleRecord(7, 4, 0)
-        libFlags = SimpleRecord(8, 4, 0)
-        version = SimpleRecord(9, 4, 0x65BE0257)
+        helpContext = IdSizeField(7, 4, 0)
+        libFlags = IdSizeField(8, 4, 0)
+        version = IdSizeField(9, 4, 0x65BE0257)
         minorVersion = PackedRecord("H", 17)
         constants = DoubleEncodedSimple(codePageName, [12, 0x003C], "")
         self.information = [
@@ -82,60 +82,6 @@ class DirStream(StreamDirectory):
         output += struct.pack("<HI", 16, 0)
         return output
 
-class SimpleRecord():
-    """
-    Many Records in this class have the same format, a two bye ID, a four byte size and an int value formatted to the defined si
-    """
-
-    def __init__(self, id, size, value):
-        self.id = id
-        self.size = size
-        self.value = value
-
-    def toDict(self):
-        return {"id": self.id, "size": self.size, "value": self.value}
-
-    def pack(self):
-        format = "<HI"
-        if isinstance(self.value, str):
-            self.stringValue = self.value
-            self.value = bytes(self.value, encoding = "ascii")
-            format += str(self.size) + "s"
-        elif isinstance(self.value, bytes):
-            format += str(self.size) + "s"
-        elif self.size == 2:
-            format += "H"
-        elif self.size == 4:
-            format += "I"
-        else:
-            raise Exception("Received data of type " + type(self.value).__name__)
-        return struct.pack(format, self.id, self.size, self.value)
-
-class SimpleValue():
-    def __init__(self, size, value):
-        self.size = size
-        self.value = value
-
-    def pack(self):
-        format = "<"
-        if self.size == 2:
-            format += "H"
-        elif self.size == 4:
-            format += "I"
-        output = struct.pack(format, self.value)
-        return output
-
-class PackedRecord():
-    """
-    A Record that is already packed
-    """
-    def __init__(self, format, *value):
-        self.value  = value
-        self.format = format
-
-    def pack(self):
-        return struct.pack("<" + self.format, *value)
-
 class DoubleEncodedSimple():
     """
     Encode text data in two successive records with different ids and lengths
@@ -149,35 +95,3 @@ class DoubleEncodedSimple():
 
     def pack(self):
         return self.modName1.pack() + self.modName2.pack()
-
-class ModuleRecord():
-    def __init__(self, codePageName, name, streamName, docString, offset, helpContext, cookie, type, readonly=False, private=False):
-        self.codePageName = codePageName
-        self.modName      = DoubleEncodedSimple(codePageName, [0x0019, 0x0047], name)
-        self.streamName   = DoubleEncodedSimple(codePageName, [0x001A, 0x0032], streamName)
-        self.docString    = DoubleEncodedSimple(codePageName, [0x001C, 0x0048], docString)
-        self.offsetRec    = SimpleRecord(0x0031, 4, offset)
-        self.helpContext  = SimpleRecord(0x001E, 4, helpContext)
-        self.cookie       = SimpleRecord(0x002C, 2, cookie)
-        self.type         = PackedRecord("HI", type, 0))
-        #self.readonly = SimpleRecord(0x001E, 4, helpContext)
-        #self.private = SimpleRecord(0x001E, 4, helpContext)
-       
-    def pack(self):
-        output = self.modName.pack() + self.streamName.pack() + self.docString.pack() + self.offsetRec.pack() + self.helpContext.pack() + self.cookie.pack() + self.type.pack()
-        footer = struct.pack("<HI", 0x002B, 0)
-        output += footer
-        return output
-
-class ReferenceRecord():
-     def __init__(self, codePageName, name, libidRef):
-         self.codePageName = codePageName
-         self.RefName = DoubleEncodedSimple(codePageName, [0x0016, 0x003E], name)
-         self.libidRef = libidRef
-
-     def pack(self):
-         strlen = len(self.libidRef)
-         format = "<HII" + str(strlen) + "sIH"
-         refRegistered = PackedRecord(struct.pack(format, 0x000D, strlen + 10, strlen, str(self.libidRef).encode(self.codePageName), 0, 0))
-
-         return self.RefName.pack() + refRegistered.pack()
