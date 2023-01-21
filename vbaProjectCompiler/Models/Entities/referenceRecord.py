cd9de7a5bea943eb4c742c306908a25b5a4605a1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-21 14:47:10 -0500

    Update referenceRecord.py

--- vbaProjectCompiler/Models/Entities/referenceRecord.py
+++ vbaProjectCompiler/Models/Entities/referenceRecord.py
@@ -1,3 +1,5 @@
+from vbaProjectCompiler.Models.Fields.libidReference import LibidReference
+
 class ReferenceRecord():
      def __init__(self, codePageName, name, libidRef):
          self.codePageName = codePageName
@@ -6,7 +8,7 @@ class ReferenceRecord():
 
      def pack(self):
          strlen = len(self.libidRef)
-         format = "<HII" + str(strlen) + "sIH"
-         refRegistered = PackedRecord(struct.pack(format, 0x000D, strlen + 10, strlen, str(self.libidRef).encode(self.codePageName), 0, 0))
+         format = "HII" + str(strlen) + "sIH"
+         refRegistered = PackedRecord(format, 0x000D, strlen + 10, strlen, str(self.libidRef).encode(self.codePageName), 0, 0)
 
          return self.RefName.pack() + refRegistered.pack()
