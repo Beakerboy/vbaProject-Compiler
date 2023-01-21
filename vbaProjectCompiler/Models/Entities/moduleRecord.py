0c6ec94fb3bc267744526e06b7ce849468f7ad6a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-21 11:14:27 -0500

    Create moduleRecord.py

---
+++ vbaProjectCompiler/Models/Entities/moduleRecord.py
@@ -0,0 +1,18 @@
+class ModuleRecord():
+    def __init__(self, codePageName, name, streamName, docString, offset, helpContext, cookie, type, readonly=False, private=False):
+        self.codePageName = codePageName
+        self.modName      = DoubleEncodedSimple(codePageName, [0x0019, 0x0047], name)
+        self.streamName   = DoubleEncodedSimple(codePageName, [0x001A, 0x0032], streamName)
+        self.docString    = DoubleEncodedSimple(codePageName, [0x001C, 0x0048], docString)
+        self.offsetRec    = SimpleRecord(0x0031, 4, offset)
+        self.helpContext  = SimpleRecord(0x001E, 4, helpContext)
+        self.cookie       = SimpleRecord(0x002C, 2, cookie)
+        self.type         = PackedRecord(struct.pack("<HI", type, 0))
+        #self.readonly = SimpleRecord(0x001E, 4, helpContext)
+        #self.private = SimpleRecord(0x001E, 4, helpContext)
+       
+    def pack(self):
+        output = self.modName.pack() + self.streamName.pack() + self.docString.pack() + self.offsetRec.pack() + self.helpContext.pack() + self.cookie.pack() + self.type.pack()
+        footer = struct.pack("<HI", 0x002B, 0)
+        output += footer
+        return output
