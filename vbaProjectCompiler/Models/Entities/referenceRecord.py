6711ef09849c7e89b618afd2826a933bc4f5f57c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-21 11:13:15 -0500

    Create referenceRecord.py

---
+++ vbaProjectCompiler/Models/Entities/referenceRecord.py
@@ -0,0 +1,12 @@
+class ReferenceRecord():
+     def __init__(self, codePageName, name, libidRef):
+         self.codePageName = codePageName
+         self.RefName = DoubleEncodedSimple(codePageName, [0x0016, 0x003E], name)
+         self.libidRef = libidRef
+
+     def pack(self):
+         strlen = len(self.libidRef)
+         format = "<HII" + str(strlen) + "sIH"
+         refRegistered = PackedRecord(struct.pack(format, 0x000D, strlen + 10, strlen, str(self.libidRef).encode(self.codePageName), 0, 0))
+
+         return self.RefName.pack() + refRegistered.pack()
