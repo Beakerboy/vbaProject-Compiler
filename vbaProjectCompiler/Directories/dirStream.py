03a2c45d4c55da00c445860cb5e7e3baa2a5568c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-21 15:24:02 -0500

    Update dirStream.py

--- vbaProjectCompiler/Directories/dirStream.py
+++ vbaProjectCompiler/Directories/dirStream.py
@@ -81,17 +81,3 @@ class DirStream(StreamDirectory):
             output += record.pack()
         output += struct.pack("<HI", 16, 0)
         return output
-
-class DoubleEncodedSimple():
-    """
-    Encode text data in two successive records with different ids and lengths
-    """
-    def __init__(self, codePageName, ids, text):
-        self.codePageName = codePageName
-        encoded = text.encode(codePageName)
-        self.modName1 = SimpleRecord(ids[0], len(encoded), encoded)
-        encoded = text.encode("utf_16_le")
-        self.modName2 = SimpleRecord(ids[1], len(encoded), encoded)
-
-    def pack(self):
-        return self.modName1.pack() + self.modName2.pack()
