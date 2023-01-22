9cb1a4014c47d3d49ab09f154632af75558b23d4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-21 20:16:46 -0500

    Update moduleRecord.py

--- vbaProjectCompiler/Models/Entities/moduleRecord.py
+++ vbaProjectCompiler/Models/Entities/moduleRecord.py
@@ -1,13 +1,15 @@
+from vbaProjectCompiler.Models.Fields.doubleEncodedString import DoubleEncodedString
+from vbaProjectCompiler.Models.Fields.packedData import PackedData
+
 class ModuleRecord():
     def __init__(self, codePageName, name, streamName, docString, offset, helpContext, cookie, type, readonly=False, private=False):
         self.codePageName = codePageName
-        self.modName      = DoubleEncodedSimple(codePageName, [0x0019, 0x0047], name)
-        self.streamName   = DoubleEncodedSimple(codePageName, [0x001A, 0x0032], streamName)
-        self.docString    = DoubleEncodedSimple(codePageName, [0x001C, 0x0048], docString)
+        self.modName      = DoubleEncodedString(codePageName, [0x001A, 0x0032], streamName)
+        self.docString    = DoubleEncodedString(codePageName, [0x001C, 0x0048], docString)
         self.offsetRec    = SimpleRecord(0x0031, 4, offset)
         self.helpContext  = SimpleRecord(0x001E, 4, helpContext)
         self.cookie       = SimpleRecord(0x002C, 2, cookie)
-        self.type         = PackedRecord(struct.pack("<HI", type, 0))
+        self.type         = PackedData("HI", type, 0)
         #self.readonly = SimpleRecord(0x001E, 4, helpContext)
         #self.private = SimpleRecord(0x001E, 4, helpContext)
        
