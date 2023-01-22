562dd11bd4a8223c2d1cec945f32b1f4f45e7c5f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-22 14:33:37 -0500

    Update test_dirObject.py

--- tests/test_dirObject.py
+++ tests/test_dirObject.py
@@ -15,5 +15,25 @@ def test_dirStream():
     readChunk = bytearray(f.read(comp.compressedChunkSize - 2))
     decompressedStream = comp.decompress(readChunk)
     stream = DirStream()
+    libidRef = LibidReference(
+            "windows",
+            "{00020430-0000-0000-C000-000000000046}",
+            "2.0",
+            "0",
+            "C:\\Windows\\System32\\stdole2.tlb",
+            "OLE Automation"
+        )
+        oleReference = ReferenceRecord(codePageName, "stdole", libidRef)
+        libidRef2 = LibidReference(
+            "windows",
+            "{2DF8D04C-5BFA-101B-BDE5-00AA0044DE52}",
+            "2.0",
+            "0",
+            "C:\\Program Files\\Common Files\\Microsoft Shared\\OFFICE16\\MSO.DLL",
+            "Microsoft Office 16.0 Object Library"
+        )
+        officeReference = ReferenceRecord(codePageName, "Office", libidRef2)
+        stream.addReference(oleReference)
+        stream.addReference(officeReference)
     expected = bytes(decompressedStream)
     assert stream.toBytes() == expected
