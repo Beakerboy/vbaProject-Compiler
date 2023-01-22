fff2debb1a6d199e491470a8ebad7953fa6f58c6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-22 17:53:10 -0500

    Update moduleRecord.py

--- vbaProjectCompiler/Models/Entities/moduleRecord.py
+++ vbaProjectCompiler/Models/Entities/moduleRecord.py
@@ -3,14 +3,14 @@ from vbaProjectCompiler.Models.Fields.packedData import PackedData
 from vbaProjectCompiler.Models.Fields.idSizeField import IdSizeField
 
 class ModuleRecord():
-    def __init__(self, codePageName, name, streamName, docString, offset, helpContext, cookie, type, readonly=False, private=False):
+    def __init__(self, codePageName, name, streamName, docString, offset, helpContext, type, readonly=False, private=False):
         self.codePageName = codePageName
         self.modName      = DoubleEncodedString(codePageName, [0x0019, 0x0047], name)
         self.streamName   = DoubleEncodedString(codePageName, [0x001A, 0x0032], streamName)
         self.docString    = DoubleEncodedString(codePageName, [0x001C, 0x0048], docString)
         self.offsetRec    = IdSizeField(0x0031, 4, offset)
         self.helpContext  = IdSizeField(0x001E, 4, helpContext)
-        self.cookie       = IdSizeField(0x002C, 2, cookie)
+        self.cookie       = IdSizeField(0x002C, 2, 0xFFFF)
         self.type         = PackedData("HI", type, 0)
         #self.readonly = SimpleRecord(0x001E, 4, helpContext)
         #self.private = SimpleRecord(0x001E, 4, helpContext)
