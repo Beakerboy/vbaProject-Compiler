4ebd35eb5bbd4a043983d6f77a7025668e1bdb21
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-21 21:12:12 -0500

    Update doubleEncodedText.py

--- vbaProjectCompiler/Models/Fields/doubleEncodedText.py
+++ vbaProjectCompiler/Models/Fields/doubleEncodedText.py
@@ -5,9 +5,9 @@ class DoubleEncodedText():
     def __init__(self, codePageName, ids, text):
         self.codePageName = codePageName
         encoded = text.encode(codePageName)
-        self.modName1 = SimpleRecord(ids[0], len(encoded), encoded)
+        self.modName1 = IdSizeField(ids[0], len(encoded), encoded)
         encoded = text.encode("utf_16_le")
-        self.modName2 = SimpleRecord(ids[1], len(encoded), encoded)
+        self.modName2 = IdSizeField(ids[1], len(encoded), encoded)
 
     def pack(self):
         return self.modName1.pack() + self.modName2.pack()
