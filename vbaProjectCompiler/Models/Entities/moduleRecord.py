b461ad378c9d1f3576ca9ce90d1b5efe6c553e8e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-23 06:17:12 -0500

    Update moduleRecord.py

--- vbaProjectCompiler/Models/Entities/moduleRecord.py
+++ vbaProjectCompiler/Models/Entities/moduleRecord.py
@@ -5,9 +5,9 @@ from vbaProjectCompiler.Models.Fields.idSizeField import IdSizeField
 class ModuleRecord():
     def __init__(self, codePageName, name, streamName, docString, offset, helpContext, type, readonly=False, private=False):
         self.codePageName = codePageName
-        self.modName      = DoubleEncodedString(codePageName, [0x0019, 0x0047], name)
-        self.streamName   = DoubleEncodedString(codePageName, [0x001A, 0x0032], streamName)
-        self.docString    = DoubleEncodedString(codePageName, [0x001C, 0x0048], docString)
+        self.modName      = DoubleEncodedString([0x0019, 0x0047], name)
+        self.streamName   = DoubleEncodedString([0x001A, 0x0032], streamName)
+        self.docString    = DoubleEncodedString([0x001C, 0x0048], docString)
         self.offsetRec    = IdSizeField(0x0031, 4, offset)
         self.helpContext  = IdSizeField(0x001E, 4, helpContext)
         self.cookie       = IdSizeField(0x002C, 2, 0xFFFF)
@@ -15,8 +15,8 @@ class ModuleRecord():
         #self.readonly = SimpleRecord(0x001E, 4, helpContext)
         #self.private = SimpleRecord(0x001E, 4, helpContext)
        
-    def pack(self):
-        output = self.modName.pack() + self.streamName.pack() + self.docString.pack() + self.offsetRec.pack() + self.helpContext.pack() + self.cookie.pack() + self.type.pack()
+    def pack(self, codePageName, endien):
+        output = self.modName.pack(codePageName, endien) + self.streamName.pack(codePageName, endien) + self.docString.pack(codePageName, endien) + self.offsetRec.pack(codePageName, endien) + self.helpContext.pack(codePageName, endien) + self.cookie.pack(codePageName, endien) + self.type.pack(codePageName, endien)
         footer = PackedData("HI", 0x002B, 0)
-        output += footer.pack()
+        output += footer.pack(codePageName, endien)
         return output
