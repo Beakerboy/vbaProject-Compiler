42815cd7e1c012d73823ed1b813e43fe1796e52c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-23 14:54:43 -0500

    Create projectWm.py

---
+++ vbaProjectCompiler/Views/projectWm.py
@@ -0,0 +1,12 @@
+class ProjectWM:
+    """
+    The ProjectWM data view for the vbaProject
+    """
+    def __init__(self, project):
+        self.project = project
+
+    def toBytes():
+        output = b''
+        for module in self.project.modules:
+            output += module.modName.value + b'\x00' + module.modName.value + b'\x00\x00'
+        output += b'\x00\x00\x00\x00'
