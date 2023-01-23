f6138d0b3578a87f052fdf4f6fca1e3834d26ace
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-23 16:14:43 -0500

    Update test_project.py

--- tests/test_project.py
+++ tests/test_project.py
@@ -14,7 +14,7 @@ def test_blank():
     project.addAttribute("HelpContextID", "0")
     project.addAttribute("VersionCompatible32", "393222000")
     project.setProtectionState("41435A5A5E5A5E5A5E5A5E")
-    project.addAttribute("DPB", "BCBEA7A2591C5A1C5A1C")
+    project.setPassword("BCBEA7A2591C5A1C5A1C")
     project.setVisibilityState("37352C2BDCDD56DE56DEA9")
 
     project.hostExtenderInfo = "&H00000001={3832D640-CF90-11CF-8E43-00A0C911005A};VBE;&H00000000"
