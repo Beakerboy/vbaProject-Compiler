83c2578c067509e4518c2d21f95e03e0e99e4524
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-23 12:12:28 -0500

    Update docModule.py

--- vbaProjectCompiler/Models/Entities/docModule.py
+++ vbaProjectCompiler/Models/Entities/docModule.py
@@ -4,9 +4,9 @@ from vbaProjectCompiler.Models.Entities.moduleRecord import ModuleRecord
 class DocModule(ModuleRecord):
 
     def __init__(self, name):
-        self.type = "Document"
         self.docTlibVer = 0
         super(DocModule, self).__init__(name)
+        self.type = "Document"
 
     def toProjectModuleString(self):
         return "Document=" + self.modName.value + "/&H" + struct.pack("<I", self.docTlibVer)
