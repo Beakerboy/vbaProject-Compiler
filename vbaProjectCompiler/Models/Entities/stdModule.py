aeeb7b9934374421b12b4326b0ccabdb62c6a7fa
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-23 12:13:03 -0500

    Update stdModule.py

--- vbaProjectCompiler/Models/Entities/stdModule.py
+++ vbaProjectCompiler/Models/Entities/stdModule.py
@@ -3,5 +3,5 @@ from vbaProjectCompiler.Models.Entities.moduleRecord import ModuleRecord
 class StdModule(ModuleRecord):
 
     def __init__(self, name):
-        self.type = "Module"
         super(StdModule, self).__init__(name)
+        self.type = "Module"
