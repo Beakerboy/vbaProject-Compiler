3cb1f784702c05a392f8b35d110b0cf1fa258dac
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-23 18:31:48 -0500

    Update projectWm.py

--- vbaProjectCompiler/Views/projectWm.py
+++ vbaProjectCompiler/Views/projectWm.py
@@ -5,7 +5,7 @@ class ProjectWM:
     def __init__(self, project):
         self.project = project
 
-    def toBytes():
+    def toBytes(self):
         output = b''
         for module in self.project.modules:
             output += module.modName.value + b'\x00' + module.modName.value + b'\x00\x00'
