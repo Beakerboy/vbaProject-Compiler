abb947ef93228595507ca092f7484fdb5b25c621
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-24 08:18:07 -0500

    Update test_vba_project.py

--- tests/Views/test_vba_project.py
+++ tests/Views/test_vba_project.py
@@ -5,10 +5,10 @@ from vbaProjectCompiler.Views.vba_Project import Vba_Project
 def test_projectWm():
     vbaProject = VbaProject()
     vba_Project = Vba_Project(vbaProject)
-    vbaProject.setPerformanceCacheVersion(0x00B5)
-    expected = b'\xCC\x61\xB5\x00\x00\x03\x00'
+    expected = b'\xCC\x61\xFF\xFF\x00\x03\x00'
     result = vba_Project.toBytes()
     assert vba_Project.toBytes() == expected
     vbaProject.setPerformanceCache(b'\x00\x01\x02\x03')
+    vbaProject.setPerformanceCacheVersion(0x00B5)
     expected = b'\xCC\x61\xB5\x00\x00\x03\x00\x00\x01\x02\x03'
     assert vba_Project.toBytes() == expected
