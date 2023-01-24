a36dd3853f451c99e6781727a8f5dc779b7a99a9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-23 21:15:20 -0500

    Update projectWm.py

--- vbaProjectCompiler/Views/projectWm.py
+++ vbaProjectCompiler/Views/projectWm.py
@@ -9,5 +9,5 @@ class ProjectWm:
         output = b''
         for module in self.project.modules:
             output += bytearray(module.modName.value, 'ascii') + b'\x00' + bytearray(module.modName.value, 'utf_16_le') + b'\x00\x00'
-        output += b'\x00\x00\x00\x00'
+        output += b'\x00\x00'
         return output
