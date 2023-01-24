ed4688b88636ea59bed149c8c7ad30fcaa994fe3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-24 12:35:09 -0500

    Update test_vba_project.py

--- tests/Views/test_vba_project.py
+++ tests/Views/test_vba_project.py
@@ -12,3 +12,11 @@ def test_projectWm():
     vbaProject.setPerformanceCacheVersion(0x00B5)
     expected = b'\xCC\x61\xB5\x00\x00\x03\x00\x00\x01\x02\x03'
     assert vba_Project.toBytes() == expected
+
+def test_realData():
+    f = open('tests/blank/vbaProject.bin', 'rb')
+    offset = 0x14C0
+    f.seek(offset)
+    data = f.read(0x0410).decode("utf_16_le")
+    assert data == "nope"
+    
