12d7fa726031315f24c61a7f4dbf982ab613a1ba
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-24 12:52:50 -0500

    Update test_vba_project.py

--- tests/Views/test_vba_project.py
+++ tests/Views/test_vba_project.py
@@ -1,5 +1,6 @@
 import pytest
 from vbaProjectCompiler.vbaProject import VbaProject
+from vbaProjectCompiler.Models.Fields.libidReference import LibidReference
 from vbaProjectCompiler.Views.vba_Project import Vba_Project
 
 def test_projectWm():
@@ -17,6 +18,31 @@ def test_realData():
     f = open('tests/blank/vbaProject.bin', 'rb')
     offset = 0x14C0
     f.seek(offset)
-    data = f.read(0x0420).decode("utf_16_le")
+    data = f.read(0x0440).decode("utf_16_le")
     assert data == "nope"
+    libs = []
+    libs.append(LibidReference(
+        "windows",
+        "{000204EF-0000-0000-C000-000000000046}",
+        "4.2",
+        9,
+        "C:\Program Files\Common Files\Microsoft Shared\VBA\VBA7.1\VBE7.DLL",
+        "Visual Basic For Applications"
+    ))
+    libs.append(LibidReference(
+        "windows",
+        "{00020813-0000-0000-C000-000000000046}",
+        "1.9",
+        0,
+        "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE",
+        "Microsoft Excel 16.0 Object Library"
+    ))
+    libs.append(LibidReference(
+        "windows",
+        "{00020430-0000-0000-C000-000000000046}",
+        "2.0",
+        0,
+        "C:\Windows\System32\stdole2.tlb",
+        "OLE Automation"
+    ))
     
