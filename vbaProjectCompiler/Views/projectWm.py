d64c4e78644cbde886760545eae31de8e1a76913
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-23 21:11:05 -0500

    Update projectWm.py

--- vbaProjectCompiler/Views/projectWm.py
+++ vbaProjectCompiler/Views/projectWm.py
@@ -8,6 +8,6 @@ class ProjectWm:
     def toBytes(self):
         output = b''
         for module in self.project.modules:
-            output += bytearray(module.modName.value, 'ascii') + b'\x00' + bytearray(module.modName.value, 'ascii') + b'\x00\x00'
+            output += bytearray(module.modName.value, 'ascii') + b'\x00' + bytearray(module.modName.value, 'utf_16_le') + b'\x00\x00'
         output += b'\x00\x00\x00\x00'
         return output
