2fe401c185cb35679d672ef5a73433d0cb18132d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-25 09:09:11 -0500

    Update and rename test_vbaProjectCompiler.py to test_fileIo.py

--- tests/test_fileIo.py
+++ tests/test_fileIo.py
@@ -2,15 +2,18 @@
 
 import pytest, binascii, sys
 from vbaProjectCompiler.vbaProject import VbaProject
+from vbaProjectCompiler.oleFile import OleFile
 
 def test_getFirstDirectoryListSector():
-    project = VbaProject()
+    vbaProject = VbaProject()
+    project = OleFile(vbaProject)
     assert project.getFirstDirectoryListSector() == 1
     project.setFirstDirectoryListSector(2)
     assert project.getFirstDirectoryListSector() == 2
 
 def test_header():
-    project = VbaProject()
+    vbaProject = VbaProject()
+    project = OleFile(vbaProject)
     result = project.header()
     expected = b'\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x3E\x00\x03\x00\xFE\xFF\x09\x00\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x10\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\xFE\xFF\xFF\xFF\x00\x00\x00\x00'
     empty = b'\xff\xff\xff\xff'
