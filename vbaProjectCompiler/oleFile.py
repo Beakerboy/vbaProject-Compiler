05ff7b624ce34583e09336478380a78a362dc2c5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-25 12:54:26 -0500

    Update oleFile.py

--- vbaProjectCompiler/oleFile.py
+++ vbaProjectCompiler/oleFile.py
@@ -113,7 +113,7 @@ class OleFile:
         """
         Calculate the number of sectors needed to express the FAT chain.
         """
-        return (len(self.fatChain) - 1) // (2 ** self.uSectorShift - 1) + 1
+        return min((len(self.fatChain) - 1) // (2 ** self.uSectorShift - 1) + 1, 1)
 
     def countDirectoryListSectors(self):
         """
