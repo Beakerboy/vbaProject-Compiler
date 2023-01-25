9dfa33dcadee2a7d5f9f906f7206b4e471a551f5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-24 20:53:53 -0500

    Update test_vba_project.py

--- tests/Views/test_vba_project.py
+++ tests/Views/test_vba_project.py
@@ -74,7 +74,7 @@ def test_realData():
     for lib in libraries:
         cache += bytearray(str(lib), "utf_16_le") + struct.pack("<IIIH", 0, 0, 0, delim[i])
         i += 1
-    cache += bytearray('\xFF' * 36, 'charmap')
+    cache += struct.pack("<HHHH", 2, 2, 1, 6, 0x0212) + bytearray('\xFF' * 36, 'charmap')
     prefix = [0x0018, 0x000C, 0x000E]
     index = 0x0046
     i = 0
