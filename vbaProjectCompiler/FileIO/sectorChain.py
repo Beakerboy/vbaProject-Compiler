02d0541e8c4e04abb6410f73dff17702133ab10f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-27 10:50:41 -0500

    Update sectorChain.py

--- vbaProjectCompiler/FileIO/sectorChain.py
+++ vbaProjectCompiler/FileIO/sectorChain.py
@@ -4,8 +4,11 @@ class SectorChain:
 
     def __init__(self):
         # The number of bytes in each sector
-        self.sectorSize = 0
+        self._sectorSize = 0
 
         # The chain.
         # Each entry points to the address of the next element in the chain.
         self.chain = []
+
+    def getSectorSize(self):
+        return self._sectorSize
