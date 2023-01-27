3f365356bb7ebc405fc952d9811d788371b94751
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-27 15:19:43 -0500

    Update test_fatChain.py

--- tests/test_fatChain.py
+++ tests/test_fatChain.py
@@ -5,8 +5,27 @@ from vbaProjectCompiler.vbaProject import VbaProject
 from vbaProjectCompiler.FileIO.fatChain import FatChain
 from vbaProjectCompiler.Directories.directory import Directory
 
-def test_defaults():
-    project = VbaProject()
+def test_initialProperties():
     chain = FatChain(512)
     assert chain.getLength() == 1
     assert chain.getChain() == [0xfffffffd]
+
+def test_addingChain():
+    chain = FatChain(512)
+    chain.startNewChain()
+    assert chain.getLength() == 2
+    assert chain.getChain() == [0xfffffffd, 0xfffffffe]
+
+    chain.startNewChain()
+    assert chain.getLength() == 3
+    assert chain.getChain() == [0xfffffffd, 0xfffffffe, 0xfffffffe]
+
+def test_extendChain():
+    chain = FatChain(512)
+    chain.startNewChain()
+    chain.startNewChain()
+    chain.extendChain(1, 2)
+    assert chain.getLength() == 4
+    assert chain.getChain() == [0xfffffffd, 3, 0xfffffffe, 4, 0xfffffffe]
+
+    
