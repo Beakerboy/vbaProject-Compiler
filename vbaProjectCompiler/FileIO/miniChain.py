3cab0eaf21ca89a8bfaf9191c5300eb91e0deab2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-27 12:14:16 -0500

    Update miniChain.py

--- vbaProjectCompiler/FileIO/miniChain.py
+++ vbaProjectCompiler/FileIO/miniChain.py
@@ -44,3 +44,15 @@ class MiniChain(SectorChain):
             firstFreeSector += 1
         self._chain.append(0xFFFFFFFE)
         return newSectors
+
+    def addStream(self, data):
+        sectors = (len(data) - 1) // self._sectorSize + 1
+        sectorNumber = len(self._chain)
+        newSectors = []
+        for i in range(sectors - 1)
+            newSectors.append(sectorNumber)
+            self._chain.append(sectorNumber + 1)
+            sectorNumber += 1
+        self._chain.append(0xFFFFFFFE)
+        newSectors.append(sectorNumber)
+        return newSectors
