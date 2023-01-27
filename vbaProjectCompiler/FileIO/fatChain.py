d499a30029543b3cc1fe5c4d25b16ec58ae8be71
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-27 10:51:10 -0500

    Update fatChain.py

--- vbaProjectCompiler/FileIO/fatChain.py
+++ vbaProjectCompiler/FileIO/fatChain.py
@@ -3,13 +3,13 @@ class FatChain(SectorChain):
 
     def writeDataToSector(self, file, sector, data = b'\x00'):
         dataLength = len(data)
-        if dataLength > self.sectorSize:
+        if dataLength > self._sectorSize:
             raise Exception("Data length is " + str(dataLength) + " bytes. Longer than a sector")
-        if dataLength < self.sectorSize:
-            data = data.ljust(self.sectorSize, b'\x00')
+        if dataLength < self._sectorSize:
+            data = data.ljust(self._sectorSize, b'\x00')
         # Check File length and fill up to the desired sector
         fileLength = file.seek(-1, os.SEEK_END)
-        desiredLength = 512 + sector * self.sectorSize
+        desiredLength = 512 + sector * self._sectorSize
         if fileLength < desiredLength:
             file.write(b'\x00' * (desiredLength - fileLength))
         file.write(data)
