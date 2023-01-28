230c44d40aa3b4705dad68634bd53d8120bfb75c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-28 09:53:24 -0500

    Update and rename test_minichain.py to test_miniChain.py

--- tests/FileIO/test_miniChain.py
+++ tests/FileIO/test_miniChain.py
@@ -3,5 +3,5 @@ from vbaProjectCompiler.FileIO.miniChain import MiniChain
 
 def test_addData():
     chain = MiniChain(64)
-    sectors = chain.addData(b'\x00')
+    sectors = chain.addStream(b'\x00')
     assert sectors == [0]
