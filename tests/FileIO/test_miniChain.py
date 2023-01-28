8bdd70d99425e0de154cbf82fa0e23aa38cfc588
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-28 09:58:21 -0500

    Update test_miniChain.py

--- tests/FileIO/test_miniChain.py
+++ tests/FileIO/test_miniChain.py
@@ -5,3 +5,10 @@ def test_addData():
     chain = MiniChain(64)
     sectors = chain.addStream(b'\x00')
     assert sectors == [0]
+
+def test_addBiggerData():
+    chain = MiniChain(64)
+    f = open("tests/blank/ThisWorkbook", "rb")
+    data = f.read()
+    sectors = chain.addStream(data)
+    assert len(sectors) == 5
