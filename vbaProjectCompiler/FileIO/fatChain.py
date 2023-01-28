7af3748a76dcaf50aa388c8f59cecb29e323cbff
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-28 09:11:00 -0500

    Update fatChain.py

--- vbaProjectCompiler/FileIO/fatChain.py
+++ vbaProjectCompiler/FileIO/fatChain.py
@@ -6,19 +6,6 @@ class FatChain(SectorChain):
         super(FatChain, self).__init__(size)
         self._chain = [0xfffffffd]
 
-    def writeDataToSector(self, file, sector, data = b'\x00'):
-        dataLength = len(data)
-        if dataLength > self._sectorSize:
-            raise Exception("Data length is " + str(dataLength) + " bytes. Longer than a sector")
-        if dataLength < self._sectorSize:
-            data = data.ljust(self._sectorSize, b'\x00')
-        # Check File length and fill up to the desired sector
-        fileLength = file.seek(-1, os.SEEK_END)
-        desiredLength = 512 + sector * self._sectorSize
-        if fileLength < desiredLength:
-            file.write(b'\x00' * (desiredLength - fileLength))
-        file.write(data)
-
     def extendChain(self, start, number):
         """
         The fat chain needs to ensure that new sectors do not land on any sectors which will be
