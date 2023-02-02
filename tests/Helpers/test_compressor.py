df46ba66b93239020712ce71ca7dc3e0996d2227
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-02 15:46:53 -0500

    Update test_compressor.py

--- tests/Helpers/test_compressor.py
+++ tests/Helpers/test_compressor.py
@@ -12,3 +12,10 @@ def test_maxCompression():
     comp = Compressor(input)
     expected = b'\x01\x03\xB0\x02\x61\x45\x00'
     assert comp.compress() == expected
+
+def test_normalCompresson():
+    input = b'#aaabcdefaaaaghijaaaaaklaaamnopqaaaaaaaaaaaarstuvwxyzaaa'
+    comp = Compressor(input)
+    expected = bytearray(b'\x01\x2F\xB0\x00\x23\x61\x61\x61\x62\x63\x64\x65\x82\x66\x00\x70\x61\x67\x68\x69\x6A\x01\x38\x08\x61\x6B\x6C\x00\x30\x6D\x6E\x6F\x70\x06\x71\x02\x70\x04\x10\x72\x73\x74\x75\x76\x10\x77\x78\x79\x7A\x00\x3C')
+    result = comp.decompress(compressed)
+    assert comp.compress() == expected
