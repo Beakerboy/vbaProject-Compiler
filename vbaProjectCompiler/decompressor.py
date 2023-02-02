0604b324ce8f90469de98de080f0901797a92c32
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-02 15:11:19 -0500

    Update decompressor.py

--- vbaProjectCompiler/decompressor.py
+++ vbaProjectCompiler/decompressor.py
@@ -216,7 +216,7 @@ class Compressor:
                 self.compressed_current = compressed_end
         else:
             if self.compressed_current < compressed_end:
-		        if not(isInstance(self.compressed_current, int)) or not(isInstance(self.decompressed_current, int)):
+                if not(isInstance(self.compressed_current, int)) or not(isInstance(self.decompressed_current, int)):
                     raise Exception("values must be integers compressed_current is " + str(self.compressed_current) + " and decomprssed_current is " + str(self.compressed_current) + ".")
 			
                 self.compressed_container[self.compressed_current] = self.data[self.decompressed_current]
