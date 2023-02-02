c323e6518a0a2a6cfba23b792499727b9fdfeeee
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-02 15:28:24 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -2,7 +2,7 @@ import pytest
 from vbaProjectCompiler.decompressor import Compressor
 
 def test_unableToCompress():
-    input = "abcdefghijklmnopqrstuv."
+    input = b'abcdefghijklmnopqrstuv.'
     comp = Compressor(input)
     expected = b'\x01\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E'
     assert comp.compress() == expected
