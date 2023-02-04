a007493d8d8ba70393b6f538aa14fcea331c9bb3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-04 17:02:35 -0500

    Update test_libidReference.py

--- tests/Unit/Models/Fields/test_libidReference.py
+++ tests/Unit/Models/Fields/test_libidReference.py
@@ -13,4 +13,4 @@ def test_str():
     expected = ("*\G{00020430-0000-0000-C000-000000000046}"
                 "#2.0#0#C:\Windows\System32\stdole2.tlb#OLE Automation")
     assert str(libidRef) == expected
-    assert len(libidRef) == 93
+    assert len(libidRef) == 94
