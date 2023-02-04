51f5f6729c0d316571568a16678110dd87768e33
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-04 17:12:01 -0500

    Update docModule.py

--- vbaProjectCompiler/Models/Entities/docModule.py
+++ vbaProjectCompiler/Models/Entities/docModule.py
@@ -1,12 +1,15 @@
-import struct
 from vbaProjectCompiler.Models.Entities.moduleRecord import ModuleRecord
 
-class DocModule(ModuleRecord):
 
+class DocModule(ModuleRecord):
+    """
+    A Document Module is a module record that is associated with a worksheet or
+    workbook.
+    """
     def __init__(self, name):
         self.docTlibVer = 0
         super(DocModule, self).__init__(name)
         self.type = "Document"
 
     def toProjectModuleString(self):
-        return "Document=" + self.modName.value + "/&H" + struct.pack(">I", self.docTlibVer).hex()
+        return "Document=" + self.modName.value + "/&H" + self.docTlibVer.to_bytes(4, "big").hex()
