e85a5d3277f6f0d914a1b9b3c87a06a12c203bc3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-05 00:00:37 -0500

    Update fatChain.py

--- vbaProjectCompiler/FileIO/fatChain.py
+++ vbaProjectCompiler/FileIO/fatChain.py
@@ -10,11 +10,15 @@ class FatChain(SectorChain):
 
 
     def getChain(self):
-        chain = super(FatChain, self).getChain()
-        if len(chain) == 0:
+        """ 0x80 should be replaced in case sector is longer
+        """
+        if len(self) == 0:
             chain = [0xFFFFFFFD]
         else:
-            pass
+            chain = super().getChain()
+            num = (len(self) - 1) // 0x80 + 1
+            for i in range(num):
+                chain[i * 0x80] = 0xFFFFFFFD
         return chain
 
 
