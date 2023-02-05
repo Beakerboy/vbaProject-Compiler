0688743f953d9ed39ad2a0cb8f517565f29c2784
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-05 18:12:10 -0500

    Update main.py

--- vbaProjectCompiler/main.py
+++ vbaProjectCompiler/main.py
@@ -2,24 +2,6 @@ from vbaProjectCompiler.directory import Directory
 from vbaProjectCompiler.vbaProject import VbaProject
 
 def main(args):
-    rootPath = args[1]
-    vbaProject = VbaProject()
-    #for each .bas file in rootPath/Modules
-    # compress file
-    # add file to the project
-
-    #for each .cls file in rootPath/ClassModules
-    # compress file
-    # add file to the project
-
-    #for each .frm file in rootPath/Forms
-    # compress file
-    # add file to the project
-
-    #Create the file
-    with open(rootPath + '/vbaProject.bin', 'w+') as file:
-        file.write(vbaProject.header())
-        #iterate along the FAT chain to write the bin
 
 def hexDumpString(data):
     decompressed = bytearray(data, encoding="charmap")
