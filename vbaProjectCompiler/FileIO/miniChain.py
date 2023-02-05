a8357e16749b5aafc0ba3c1a011b5a54da276f9d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-04 21:16:38 -0500

    Update miniChain.py

--- vbaProjectCompiler/FileIO/miniChain.py
+++ vbaProjectCompiler/FileIO/miniChain.py
@@ -1,15 +1,57 @@
 import os
 from vbaProjectCompiler.FileIO.sectorChain import SectorChain
-class MiniChain(SectorChain):
+from vbaProjectCompiler.Models.Entities.Streams.streamBase import StreamBase
 
-    def addStream(self, data):
-        sectors = (len(data) - 1) // self._sectorSize + 1
-        sectorNumber = len(self._chain)
-        newSectors = []
-        for i in range(sectors - 1):
-            newSectors.append(sectorNumber)
-            self._chain.append(sectorNumber + 1)
-            sectorNumber += 1
-        self._chain.append(0xFFFFFFFE)
-        newSectors.append(sectorNumber)
-        return newSectors
+
+class MiniChain(SectorChain, StreamBase):
+
+    def addStream(self, stream):
+        """
+        Add a new stream to the minifat chaain and arrange the storage resources
+        We need to manage changes to the minifat chain, minifat stream, and the
+        FAT resources for them.
+        """
+
+        # If we have not started a minifat data stream in the FAT chain
+        # start one now.
+        if len(self._streams) == 0:
+            self._streams = StreamBase()
+            self._storageChain.addStream(self._streams)
+
+        # Create a new entry on the minifat chain and tell the stream what it is
+        sector = self.startNewChain()
+        stream.setStartSector(sector)
+
+        # Figure out how many more are needed and pass it on to the stream
+        sectorsNeeded = (stream.getSize() - 1) // self._sectorSize + 1
+        additionalSectors = self.extendChain(sector, sectorsNeeded)
+        stream.setAdditionalSectors(additionalSectors)
+        self._streams.append(stream)
+
+
+    def extendChain(self, stream, number):
+        """
+        """
+        pass
+
+
+    def startNewChain(self):
+        # Increase the necessary chain resources by one address
+        newSector = self._nextFreeSector
+        self.append(1)
+        return newSector
+
+ 
+    def streamSize(self):
+        """
+        implementation of StreamBase.streamSize()
+        """
+        pass
+
+
+    def _extendData(self, number):
+        """
+        implementation of StreamBase._extendData()
+        """
+        self._nextFreeSector += data
+
