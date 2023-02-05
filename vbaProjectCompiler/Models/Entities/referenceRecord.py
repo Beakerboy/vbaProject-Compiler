34be48e640bb2d1f4137be35f8a8c0229276003a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-05 17:53:22 -0500

    Update referenceRecord.py

--- vbaProjectCompiler/Models/Entities/referenceRecord.py
+++ vbaProjectCompiler/Models/Entities/referenceRecord.py
@@ -1,10 +1,11 @@
 from vbaProjectCompiler.Models.Fields.libidReference import LibidReference
-
 from vbaProjectCompiler.Models.Fields.doubleEncodedString import DoubleEncodedString
 from vbaProjectCompiler.Models.Fields.packedData import PackedData
 from vbaProjectCompiler.Models.Fields.idSizeField import IdSizeField
 
+
 class ReferenceRecord():
+
      def __init__(self, codePageName, name, libidRef):
          self.codePageName = codePageName
          self.RefName = DoubleEncodedString([0x0016, 0x003E], name)
@@ -13,6 +14,9 @@ class ReferenceRecord():
      def pack(self, codePageName, endien):
          strlen = len(self.libidRef)
          format = "HII" + str(strlen) + "sIH"
-         refRegistered = PackedData(format, 0x000D, strlen + 10, strlen, str(self.libidRef).encode(self.codePageName), 0, 0)
+         lib_str = str(self.libidRef).encode(self.codePageName)
+         refRegistered = PackedData(format, 0x000D, strlen + 10,
+                                    strlen, lib_str, 0, 0)
 
-         return self.RefName.pack(codePageName, endien) + refRegistered.pack(codePageName, endien)
+         return (self.RefName.pack(codePageName, endien)
+                 + refRegistered.pack(codePageName, endien))
