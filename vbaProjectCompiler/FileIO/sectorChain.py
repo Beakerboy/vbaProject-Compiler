5c25e5793376eaa9c5a603fe8123ccdca06cef6f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-04 22:54:52 -0500

    Update sectorChain.py

--- vbaProjectCompiler/FileIO/sectorChain.py
+++ vbaProjectCompiler/FileIO/sectorChain.py
@@ -68,9 +68,14 @@ class SectorChain:
 
 
     def addStream(self, stream):
-        sector = self._reserveNextFreeSector(self)
+        sector = self._startNewChain(self)
         stream.setStartSector(sector)
         sectorsNeeded = (stream.getSize() - 1) // self._sectorSize + 1
         additionalSectors = self.extendChain(stream, sectorsNeeded)
         stream.setAdditionalSectors(additionalSectors)
         self._streams.append(stream)
+
+    def _startNewChain(self):
+        # Increase the necessary chain resources by one address
+        newSector = self._reserveNextFreeSector()
+        return newSector
