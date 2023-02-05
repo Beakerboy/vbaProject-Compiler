c44389e931e925cf7727809bc5932e9211e9fb6f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-04 21:38:45 -0500

    Update fatChain.py

--- vbaProjectCompiler/FileIO/fatChain.py
+++ vbaProjectCompiler/FileIO/fatChain.py
@@ -2,65 +2,8 @@ import os
 from vbaProjectCompiler.FileIO.sectorChain import SectorChain
 class FatChain(SectorChain):
 
-    def __init__(self, size):
-        super(FatChain, self).__init__(size)
-        self._chain = [0xfffffffd]
-
-    def extendChain(self, start, number):
-        """
-        The fat chain needs to ensure that new sectors do not land on any sectors which will be
-        for fat chain information.
-        """
-        if number < 1:
-            raise Exception("Number must be greater than zero.")
-        if start >= len(self._chain):
-            raise Exception("Chain does not exist at that address.")
-        if self._chain[start] == 0xFFFFFFFD:
-            raise Exception("Unable to start on a Fat Chain sector.")
-        newSectors = []
-        lastSector = start
-        nextSector = start
-        while nextSector != 0xFFFFFFFE:
-            lastSector = nextSector
-            nextSector = self._chain[lastSector]
-        firstFreeSector = len(self._chain)
-        # Check That we are not reserving what should be a FAT sector
-        if firstFreeSector % 0x80 == 0:
-            self._chain.append(0xFFFFFFFD)
-            firstFreeSector += 1
-        self._chain[lastSector] = firstFreeSector
-        newSectors.append(firstFreeSector)
-        for i in range(number - 1):
-            firstFreeSector += 1
-            if firstFreeSector % 0x80 == 0:
-                firstFreeSector += 1
-                self._chain.append(firstFreeSector)
-                self._chain.append(0xFFFFFFFD)
-            else:
-                self._chain.append(firstFreeSector)
-            newSectors.append(firstFreeSector)
-        self._chain.append(0xFFFFFFFE)
-        newSectors.append(len(self._chain) - 1)
-        return newSectors
-
-    def startNewChain(self):
-        """
-        We assume sectors are being filled sequentially.
-        Start a new single sector chain and return the address.
-        """
-        if len(self._chain) % 0x80 == 0:
-            self._chain.append(0xFFFFFFFD)
-        self._chain.append(0xfffffffe)
-        return len(self._chain) - 1
-
-    def findSector(self, startSector, depth):
-        """
-        Follow the fat chain starting at startSector, for depth {depth} to find the sector.
-        depth is zero or greater.
-        """
-        sector = startSector
-        for i in range(depth):
-            sector = self._chain[sector]
-            if sector == 0xFFFFFFFE:
-                return sector
-        return sector
+    def reserveNextFreeSector(self):
+        sector = self._nextFreeSector
+        self._nextFreeSector +=1
+        if self._nextFreeSector % 0x80 ==0:
+            self._nextFreeSector +=1
