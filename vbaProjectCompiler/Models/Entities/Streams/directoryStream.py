a82850c65b56cf15d9689c7d463c2bdd9d0c32b1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-04 19:42:31 -0500

    Update directoryStream.py

--- vbaProjectCompiler/Models/Entities/Streams/directoryStream.py
+++ vbaProjectCompiler/Models/Entities/Streams/directoryStream.py
@@ -1,4 +1,9 @@
 from vbaProjectCompiler.Models.Entities.Streams.streamBase import StreamBase
 
+
 class DirectoryStream(StreamBase):
+    """
+    The directory stream is an arrary of directory records
+    """
+
     pass
