febdeb5a5c3f723512c6f17eaa296cbe7f4e2eab
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-04 21:47:32 -0500

    Update fatChain.py

--- vbaProjectCompiler/FileIO/fatChain.py
+++ vbaProjectCompiler/FileIO/fatChain.py
@@ -2,6 +2,11 @@ import os
 from vbaProjectCompiler.FileIO.sectorChain import SectorChain
 class FatChain(SectorChain):
 
+    def __init__(self):
+        super(fatChain, self).__init__()
+        self._nextFreeSector = 1
+
+
     def reserveNextFreeSector(self):
         sector = self._nextFreeSector
         self._nextFreeSector +=1
