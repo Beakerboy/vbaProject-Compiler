d65b6ea65710feb0c971c0b313b5138eb550aad5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-04 19:54:46 -0500

    Update streamBase.py

--- vbaProjectCompiler/Models/Entities/Streams/streamBase.py
+++ vbaProjectCompiler/Models/Entities/Streams/streamBase.py
@@ -1,27 +1,46 @@
 
 class StreamBase:
-   """
-   Base class for any object which will appear as a stream within a sector chain
-   """
+    """
+    Base class for any object which will appear as a stream within a sector chain
+    """
 
-   def __init__(self):
-       self._data = b''
+    def __init__(self):
 
+        # The stuff that will be used to squeeze data into the chain.
+        # It can just be the data itself.
+        self._data = b''
 
-   def setStorageChain(self, chain):
-       self._storageChain = chain
+        # An array of sectors this stream will reside
+        self._additionalSectors = []
 
-
-   def append(self, data):
-       """
-       Extend the data in this stream. Request additional chain storage if needed
-       """
-       self._extendData(data)
-       self._storageChain.requestNewSectors(len(self._additionalSectors) + 1, self.streamSize())
+        # An object of type SectorChain which will be storing this stream
+        self._storageChain = 0
 
 
-   def _extendData(self, data):
-       """
-       Add new data to the bytearray
-       """
-       self._data += data
+    def setStorageChain(self, chain):
+        self._storageChain = chain
+
+
+    def append(self, data):
+        """
+        Extend the data in this stream. Request additional chain storage if needed
+        """
+        self._extendData(data)
+        currentSecCnt = len(self._additionalSectors) + 1
+        newSectors = self._storageChain.requestNewSectors(currentSecCnt, self.streamSize())
+        if len(newSectors) > 0:
+            self._additionalSectors.append(newSectors)
+
+
+    def streamSize(self):
+        """
+        The size the stream will be when rendered
+        ""
+        return len(self._data)
+
+
+    def _extendData(self, data):
+        """
+        Add new data to the bytearray
+        """
+        self._data += data
