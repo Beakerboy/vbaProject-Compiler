c08113a574c252511accdab9b56e7335e0cfc6e5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-05 22:05:45 -0500

    Update moduleRecord.py

--- vbaProjectCompiler/Models/Entities/moduleRecord.py
+++ vbaProjectCompiler/Models/Entities/moduleRecord.py
@@ -14,8 +14,8 @@ class ModuleRecord():
         self.helpContext  = IdSizeField(0x001E, 4, 0)
         self.cookie       = IdSizeField(0x002C, 2, 0xFFFF)
        
-        #self.readonly = SimpleRecord(0x001E, 4, helpContext)
-        #self.private = SimpleRecord(0x001E, 4, helpContext)
+        # self.readonly = SimpleRecord(0x001E, 4, helpContext)
+        # self.private = SimpleRecord(0x001E, 4, helpContext)
         self.cache = b''
         self.workspace = [0, 0, 0, 0, 'C']
         self.type = ''
@@ -41,7 +41,13 @@ class ModuleRecord():
         typeIdValue = 0x0022 if self.type == 'Document' else 0x0021
         typeId = PackedData("HI", typeIdValue, 0) 
         self.offsetRec = IdSizeField(0x0031, 4, len(self.cache))
-        output = self.modName.pack(codePageName, endien) + self.streamName.pack(codePageName, endien) + self.docString.pack(codePageName, endien) + self.offsetRec.pack(codePageName, endien) + self.helpContext.pack(codePageName, endien) + self.cookie.pack(codePageName, endien) + typeId.pack(codePageName, endien)
+        output = (self.modName.pack(codePageName, endien)
+                  + self.streamName.pack(codePageName, endien)
+                  + self.docString.pack(codePageName, endien)
+                  + self.offsetRec.pack(codePageName, endien)
+                  + self.helpContext.pack(codePageName, endien)
+                  + self.cookie.pack(codePageName, endien)
+                  + typeId.pack(codePageName, endien))
         footer = PackedData("HI", 0x002B, 0)
         output += footer.pack(codePageName, endien)
         return output
