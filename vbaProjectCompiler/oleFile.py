0a9d402bca40ef0f8a094ea96bb61b25624fd2cc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-05 21:50:25 -0500

    Update oleFile.py

--- vbaProjectCompiler/oleFile.py
+++ vbaProjectCompiler/oleFile.py
@@ -4,7 +4,9 @@ from vbaProjectCompiler.Directories.directory import Directory
 from vbaProjectCompiler.Directories.rootDirectory import RootDirectory
 from vbaProjectCompiler.FileIO.fatChain import FatChain
 from vbaProjectCompiler.FileIO.miniChain import MiniChain
-from vbaProjectCompiler.Models.Entites.Streams.directoryStream import DirectoryStream
+from vbaProjectCompiler.Models.Entites.Streams.directoryStream import (
+    DirectoryStream
+)
 
 
 class OleFile:
