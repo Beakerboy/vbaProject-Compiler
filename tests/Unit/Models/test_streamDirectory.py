4af3ee8a3eb2cf497f6715234737f1aeaba082ec
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-06 22:19:57 -0500

    Update test_streamDirectory.py

--- tests/Unit/Models/test_streamDirectory.py
+++ tests/Unit/Models/test_streamDirectory.py
@@ -1,5 +1,7 @@
 from vbaProjectCompiler.Directories.streamDirectory import StreamDirectory
-
+from vbaProjectCompiler.Models.Fields.doubleEncodedString import (
+    DoubleEncodedString
+)
 
 def test_StreamDirectory():
     stub = ModuleStub()
@@ -10,4 +12,4 @@ def test_StreamDirectory():
 class ModuleStub():
 
     def __init__(self):
-        self.modName = "stub"
+        self.modName = DoubleEncodedString([0x0019, 0x0047], "stub")
