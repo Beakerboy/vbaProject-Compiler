43645678cbcc06330ac574ec89f5536ea27781f7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-06 22:23:15 -0500

    Update test_streamDirectory.py

--- tests/Unit/Models/test_streamDirectory.py
+++ tests/Unit/Models/test_streamDirectory.py
@@ -8,9 +8,13 @@ def test_StreamDirectory():
     stub = ModuleStub()
     dir = StreamDirectory.createFromModule(stub)
     assert dir.type == 2
+    assert dir.getData() == "foo"
 
 
 class ModuleStub():
 
     def __init__(self):
         self.modName = DoubleEncodedString([0x0019, 0x0047], "stub")
+
+    def getData(self):
+        return "foo"
