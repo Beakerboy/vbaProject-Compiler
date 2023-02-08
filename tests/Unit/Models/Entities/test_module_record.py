35878180a1bf3b66485560ae3692923a314c185d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-07 22:35:30 -0500

    Update test_module_record.py

--- tests/Unit/Models/Entities/test_module_record.py
+++ tests/Unit/Models/Entities/test_module_record.py
@@ -1,6 +1,8 @@
 from vbaProjectCompiler.Models.Entities.moduleRecord import ModuleRecord
 
+
 def test_constructor():
     module = ModuleRecord("foo")
     path = "vbaProjectCompiler/blank_files/ThisWorkbook.cls"
     module.add_file(path)
+    
