bcc39097e59554a9f49841f0fa97315c6e8921ad
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-07 22:52:24 -0500

    Update test_module_record.py

--- tests/Unit/Models/Entities/test_module_record.py
+++ tests/Unit/Models/Entities/test_module_record.py
@@ -6,7 +6,7 @@ def test_constructor():
     path1 = "vbaProjectCompiler/blank_files/ThisWorkbook.cls"
     module.add_file(path1)
     module.normalize_file()
-    f = open(path1, "r")
+    f = open(path1 + ".new", "r")
     path2 = "tests/blank/ThisWorkbook"
     e = open(path2, "r")
     while line := f.readline():
