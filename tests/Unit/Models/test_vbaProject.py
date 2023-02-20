f48b5df35878d46d650e58cb71df2a5782dfb056
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-20 12:46:19 -0500

    Update test_vbaProject.py

--- tests/Unit/Models/test_vbaProject.py
+++ tests/Unit/Models/test_vbaProject.py
@@ -5,7 +5,7 @@ from vbaProjectCompiler.vbaProject import VbaProject
 test_set_get_visibility():
     project = VbaProject()
     project.set_visibility_state(0)
-    assert project.set_visibility_state() == 0
+    assert project.get_visibility_state() == 0
 
 def test_bad_visibility():
     """
