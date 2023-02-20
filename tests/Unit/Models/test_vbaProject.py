99705d3e6d6455278cc5bd58c6132a7d3120586b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-20 12:46:05 -0500

    Update test_vbaProject.py

--- tests/Unit/Models/test_vbaProject.py
+++ tests/Unit/Models/test_vbaProject.py
@@ -2,6 +2,11 @@ import pytest
 from vbaProjectCompiler.vbaProject import VbaProject
 
 
+test_set_get_visibility():
+    project = VbaProject()
+    project.set_visibility_state(0)
+    assert project.set_visibility_state() == 0
+
 def test_bad_visibility():
     """
     Visibility must be zero or 0xFF
