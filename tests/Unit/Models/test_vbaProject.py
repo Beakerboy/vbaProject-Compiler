fe4dc0c9c6d6ad21a8cb7d07fd07d71422f70830
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-20 13:10:56 -0500

    Update test_vbaProject.py

--- tests/Unit/Models/test_vbaProject.py
+++ tests/Unit/Models/test_vbaProject.py
@@ -8,6 +8,18 @@ def test_set_get_visibility():
     assert project.get_visibility_state() == 0
 
 
+def test_set_get_protection():
+    project = VbaProject()
+    project.set_protection_state(0)
+    assert project.get_protection_state() == 0
+
+
+def test_set_get_password():
+    project = VbaProject()
+    project.set_password(0)
+    assert project.get_password() == 0
+
+
 def test_bad_visibility():
     """
     Visibility must be zero or 0xFF
