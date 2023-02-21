2e001fe504ee7f1d084e516bdb9181b7f7951d8e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-21 12:10:28 -0500

    Create test_helper.py

---
+++ tests/Functional/test_helper.py
@@ -0,0 +1,12 @@
+test_create_cache():
+    # Read the data from the demo file and decompress it.
+    f = open('tests/blank/vbaProject.bin', 'rb')
+    offset = 0x0800
+    length = 0x0333
+    f.seek(offset)
+    expected = f.read(length)
+
+    cookie = 0xB81C
+    guid = "{00020819-0000-0000-C000-000000000046}"
+    cache = create_cache.create_cache(cookie, guid)
+    assert cache == expected
