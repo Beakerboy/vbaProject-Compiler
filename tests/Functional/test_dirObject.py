39edda3340beda6a692df265d0438bd04bf3489f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-21 12:19:49 -0500

    Update test_dirObject.py

--- tests/Functional/test_dirObject.py
+++ tests/Functional/test_dirObject.py
@@ -48,7 +48,7 @@ def test_dirStream():
 
     thisWorkbook = DocModule("ThisWorkbook")
     thisWorkbook.cookie.value = 0xB81C
-    guid = "0{00020819-0000-0000-C000-000000000046}"
+    guid = "{00020819-0000-0000-C000-000000000046}"
     cache = create_cache.create_cache(thisWorkbook.cookie.value, guid)
     thisWorkbook.addPerformanceCache(cache)
 
