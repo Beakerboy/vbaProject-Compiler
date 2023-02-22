a86663e27657d75370745adebc70f6b996414e7b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-22 18:04:45 -0500

    Update docModule.py

--- vbaProjectCompiler/Models/Entities/docModule.py
+++ vbaProjectCompiler/Models/Entities/docModule.py
@@ -29,16 +29,16 @@ class DocModule(ModuleRecord):
         guid = '0' + self._vbBase
         guid_bytes = bytes(guid, "utf_16_le")
         guid_size = len(guid_bytes).to_bytes(2, "little")
-        ms = (b'\x00\x00\x02\x00'
-          + b'\x53\x4C\xFF\xFF\xFF\xFF\x00\x00\x01\x00\x53\x10\xFF\xFF\xFF\xFF'
-          + b'\x00\x00\x01\x00\x53\x94\xFF\xFF\xFF\xFF\x00\x00\x00\x00\x02\x3C'
-          + b'\xFF\xFF\xFF\xFF')
-        ca = self._create_cache_header(self.cookie, b'\xD2', b'\x00\x02', b'\xD9',
-                               b'\x2D\x03', b'\x23\x01', b'\x08', b'\x18')
+        data = ("00 00 02 00",
+                "53 4C FF FF FF FF 00 00 01 00 53 10 FF FF FF FF",
+                "00 00 01 00 53 94 FF FF FF FF 00 00 00 00 02 3C",
+                "FF FF FF FF")
+        ca = self._create_cache_header(self.cookie, b'\xD2', b'\x00\x02',
+                                       b'\xD9', b'\x2D\x03', b'\x23\x01',
+                                       b'\x08', b'\x18')
         data1 = (guid_size + guid_bytes)
-        d2 = (b''
-          + b'\x02\x80\xFE\xFF\xFF\xFF\xFF\xFF\x20\x00\x00\x00\xFF\xFF\xFF\xFF'
-          + b'\x30\x00\x00\x00\x02\x01\xFF\xFF\x00\x00\x00\x00\x00\x00\x00\x00'
+        d2 = ("02\x80\xFE\xFF\xFF\xFF\xFF\xFF\x20\x00\x00\x00\xFF\xFF\xFF\xFF'
+              "30\x00\x00\x00\x02\x01\xFF\xFF\x00\x00\x00\x00\x00\x00\x00\x00'
           + b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x00\x00\x00\x00\x2E\x00\x43\x00'
           + b'\x1D\x00\x00\x00\x25\x00\x00\x00\xFF\xFF\xFF\xFF\x40\x00\x00\x00'
           + b'')
