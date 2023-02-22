77ba433e06e65184bb1f1c2ace85bf1564b92692
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-22 18:29:15 -0500

    Update docModule.py

--- vbaProjectCompiler/Models/Entities/docModule.py
+++ vbaProjectCompiler/Models/Entities/docModule.py
@@ -37,11 +37,11 @@ class DocModule(ModuleRecord):
                                        b'\xD9', b'\x2D\x03', b'\x23\x01',
                                        b'\x08', b'\x18')
         data1 = (guid_size + guid_bytes)
-        d2 = ("02 80 FE FF FF FF FF FF 20 00 00 00 FF FF FF FF"
-              "30\x00\x00\x00\x02\x01\xFF\xFF\x00\x00\x00\x00\x00\x00\x00\x00'
-              "FF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x00\x00\x00\x00\x2E\x00\x43\x00'
-              "1D 00 00 00 25 00 00 00 FF FF FF FF 40 00 00 00")
-        ca = (ca + self._create_cache_middle(ms, data1, d2)
+        data2= ("02 80 FE FF FF FF FF FF 20 00 00 00 FF FF FF FF",
+                "30 00 00 00 02 01 FF FF 00 00 00 00 00 00 00 00",
+                "FF FF FF FF FF FF FF FF 00 00 00 00 2E 00 43 00",
+                "1D 00 00 00 25 00 00 00 FF FF FF FF 40 00 00 00")
+        ca = (ca + self._create_cache_middle(data, data1, data2)
           + b'\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00'
           + self._create_cache_footer(b'\00'))
         return ca
