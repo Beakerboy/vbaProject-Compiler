ada43ccfd67b1bd2f522f541c02a6dbba7a4b531
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-23 10:24:54 -0500

    Update test_docModule.py

--- tests/Unit/Models/Entities/test_docModule.py
+++ tests/Unit/Models/Entities/test_docModule.py
@@ -12,3 +12,26 @@ def test_create_cache():
     f.seek(0x0800)
     file_data = f.read(0x0333)
     assert this_workbook.get_cache() == file_data
+
+
+def test_create_cache2():
+    module = DocModule("Sheet1")
+    module.cookie.value = 0x9B9A
+    guid = "{00020820-0000-0000-C000-000000000046}"
+    module.addVbBase(guid)
+    module.create_cache()
+
+    f = open('tests/blank/vbaProject.bin', 'rb')
+    f.seek(0x0C00)
+    file_data = f.read(0x0333)
+    assert module.get_cache() == file_data
+
+def test_create_cache3():
+    module = StdModule("Module1")
+    module.cookie.value = 0xB241
+    module.create_cache()
+
+    f = open('tests/blank/vbaProject.bin', 'rb')
+    f.seek(0x1200)
+    file_data = f.read(0x0283)
+    assert module.get_cache() == file_data
