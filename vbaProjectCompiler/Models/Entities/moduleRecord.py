fcd3461221ff626b7057e09b8dd838abe13536f0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-23 19:26:31 -0500

    Update moduleRecord.py

--- vbaProjectCompiler/Models/Entities/moduleRecord.py
+++ vbaProjectCompiler/Models/Entities/moduleRecord.py
@@ -142,7 +142,7 @@ class ModuleRecord():
               "00 00 00 00 00")
         return bytes.fromhex(" ".join(fo))
 
-    def _create_pcode()->bytes:
+    def _create_pcode(self)->bytes:
         pcode = ("FE CA 01 00 00 00 FF FF FF FF 01",
                  "01 08 00 00 00 FF FF FF FF 78 00 00 00 FF FF FF",
                  "FF 00 00")
