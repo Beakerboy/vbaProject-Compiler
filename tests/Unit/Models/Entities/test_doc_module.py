f8540c6e8a6f759cb7c83e5b58f161ae3ed9ac7e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-24 11:29:43 -0500

    Update test_doc_module.py

--- tests/Unit/Models/Entities/test_doc_module.py
+++ tests/Unit/Models/Entities/test_doc_module.py
@@ -32,6 +32,8 @@ def test_normalize():
     module = DocModule("foo")
     path1 = "vbaProjectCompiler/blank_files/ThisWorkbook.cls"
     module.add_file(path1)
+    guid = "{00020819-0000-0000-C000-000000000046}"
+    this_workbook.set_guid(guid)
     module.normalize_file()
     f = open(path1 + ".new", "r")
     path2 = "tests/blank/ThisWorkbook"
