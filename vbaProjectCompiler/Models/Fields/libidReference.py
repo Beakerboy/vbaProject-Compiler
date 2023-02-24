5d998c6c6a1756f49d1b4cdbe05405410b4170f8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-24 13:44:14 -0500

    Update libidReference.py

--- vbaProjectCompiler/Models/Fields/libidReference.py
+++ vbaProjectCompiler/Models/Fields/libidReference.py
@@ -1,12 +1,16 @@
+from pathlig import Path
 class LibidReference():
-    def __init__(self, pathType, libidGuid, version,
+    def __init__(self, libidGuid, version,
                  libidLcid, libidPath, libidRegName):
-        self.libidReferenceKind = "G" if pathType == "windows" else "H"
         self.libidGuid = libidGuid
         self.version = version
         self.libidLcid = libidLcid
-        self.libidPath = libidPath
+        self.libidPath = Path(libidPath)
         self.libidRegName = libidRegName
+        if type(self.libidPath) == "WindowsPath" or type(self.libidPath) == "PureWindowsPath":
+            self.libidReferenceKind = "G"
+        else:
+            self.libidReferenceKind = "H"
 
     def __str__(self):
         return "*\\" + \
