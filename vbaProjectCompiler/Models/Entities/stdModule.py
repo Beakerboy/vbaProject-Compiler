6c9358d9a622a63c6b46445d44aba4c26053148c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-24 08:11:03 -0500

    Update stdModule.py

--- vbaProjectCompiler/Models/Entities/stdModule.py
+++ vbaProjectCompiler/Models/Entities/stdModule.py
@@ -11,7 +11,7 @@ class StdModule(ModuleRecord):
         id_table = 0x0188.to_bytes(2, "little")
         ca = self._create_cache_header(self.cookie, b'\x22', id_table,
                                        b'\x7D\x02', b'\x03\x00',
-                                       b'\x00', b'\x02', b'\x00')
+                                       b'\x00', b'\x02', b'\xFF\xFF')
         data2 = b'\xFF\xFF\xFF\xFF\x78\x00\x00\x00'
         ca = (ca + self._create_cache_middle(b'', [], data2)
               + b'\x00\x00'
