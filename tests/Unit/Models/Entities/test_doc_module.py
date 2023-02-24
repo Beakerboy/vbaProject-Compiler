5ae33342c8bb1cb333bd75c4b6f4ed839df6e044
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-24 11:36:21 -0500

    Cover Normalize (#88)
    * Cover docModule.normalize()

--- tests/Unit/Models/Entities/test_doc_module.py
+++ tests/Unit/Models/Entities/test_doc_module.py
@@ -1,3 +1,4 @@
+from ms_ovba_compression.ms_ovba import MsOvba
 from vbaProjectCompiler.Models.Entities.doc_module import DocModule
 
 
@@ -25,3 +26,28 @@ def test_create_cache2():
     f.seek(0x0C00)
     file_data = f.read(0x0333)
     assert module.get_cache() == file_data
+
+
+def test_normalize():
+    module = DocModule("foo")
+    path1 = "vbaProjectCompiler/blank_files/ThisWorkbook.cls"
+    module.add_file(path1)
+    guid = "{00020819-0000-0000-C000-000000000046}"
+    module.set_guid(guid)
+    module.normalize_file()
+    f = open(path1 + ".new", "r")
+    path2 = "tests/blank/ThisWorkbook"
+    e = open(path2, "r")
+    while line := f.readline():
+        assert line == e.readline()
+    path3 = path1 + ".bin"
+    f_stream = open(path3, "rb")
+    full_binary = open('tests/blank/vbaProject.bin', 'rb')
+    offset = 0x0B33
+    length = 0x00B4
+    full_binary.seek(offset)
+    container = full_binary.read(length)
+    ms_ovba = MsOvba()
+    test_data = ms_ovba.decompress(f_stream.read())
+    expected_data = ms_ovba.decompress(container)
+    assert test_data == expected_data
