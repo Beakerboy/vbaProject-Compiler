d3ea33360b1e70ed5472cf6d94e4a9b2ffee6458
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-24 10:58:51 -0500

    Update test_datetime.py

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -2,6 +2,7 @@ import datetime
 
 
 def test_datetime():
+    # use a library
     input = 0x01D92433C2B823C0
     date = filetime2datetime(input)
     assert date.ctime() == "Mon Jan  9 14:07:51 2023"
