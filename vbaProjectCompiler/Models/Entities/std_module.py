df0db1897690c70e2a6d6bc91df05ca9007d346f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-27 11:46:42 -0500

    Update std_module.py

--- vbaProjectCompiler/Models/Entities/std_module.py
+++ vbaProjectCompiler/Models/Entities/std_module.py
@@ -10,7 +10,7 @@ class StdModule(ModuleBase):
 
     def create_cache(self) -> bytes:
         cache = ModuleCache()
-        cache.cookie = 0xB241
+        cache.cookie = self.cookie.value
         cache.misc = [0x0316, 0x0222, 0x027D, 3, 0, 2, 0xFFFF, "FFFFFFFF", 0]
         cache.indirect_table = struct.pack("<iI", -1, 0x78)
 
