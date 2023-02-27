31cbdf55d8452818d77d964db56198f3482b1ff5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-27 10:02:56 -0500

    Update test_module_cache.py

--- tests/Unit/Models/Entities/test_module_cache.py
+++ tests/Unit/Models/Entities/test_module_cache.py
@@ -8,7 +8,6 @@ def test_module_cache():
     cache.misc = [0x0316, 0x02D2, 0x032D, 0x0123, 0x88, 8, 0x18, "00000000", 1]
     guid = uuid.UUID('0002081900000000C000000000000046')
     cache.guid = bytes(("0{" + str(guid) + "}").upper(), "utf_16_le")
-    cache.guids1 = b'\xff' * 4 + b'\x00' * 54
 
     indirect_table = ("02 80 FE FF FF FF FF FF 20 00 00 00 FF FF FF FF",
                       "30 00 00 00 02 01 FF FF 00 00 00 00 00 00 00 00",
@@ -24,4 +23,14 @@ def test_module_cache():
     f = open('tests/blank/vbaProject.bin', 'rb')
     f.seek(0x0800)
     file_data = f.read(0x0333)
+    assert cache.to_bytes() == file_data
+
+def test_create_cache():
+    cache = ModuleCache()
+    cache.cookie = 0xB241
+    cache.misc = [0x1602, 0x027D', 3, 0, 2, 0xFFFF "FFFFFFFF", 0]
+    cache.indirect_table = struct.pack("<iI", -1, 0x78)
+    f = open('tests/blank/vbaProject.bin', 'rb')
+    f.seek(0x1200)
+    file_data = f.read(0x0283)
     assert cache.to_bytes() == file_data
