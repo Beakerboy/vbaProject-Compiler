b0e8d4b985c98ea7cce5775cdf8616b7f343648b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-28 16:05:18 -0500

    Update test_doc_module.py

--- tests/Unit/Models/Entities/test_doc_module.py
+++ tests/Unit/Models/Entities/test_doc_module.py
@@ -1,10 +1,30 @@
 import os
 import uuid
 from ms_ovba_compression.ms_ovba import MsOvba
+from ms_pcode_assembler.module_cache import ModuleCache
 from vbaProjectCompiler.Models.Entities.doc_module import DocModule
 
 
 def test_normalize():
+
+    cache = ModuleCache()
+    cache.module_cookie = 0xB81C
+    cache.project_cookie = 0x08F3
+    cache.misc = [0x0316, 0x032D, 0x0123, 0x88, 8, 0x18, "00000000", 1]
+    guid = uuid.UUID('0002081900000000C000000000000046')
+    cache.guid = bytes(("0{" + str(guid) + "}").upper(), "utf_16_le")
+
+    indirect_table = ("02 80 FE FF FF FF FF FF 20 00 00 00 FF FF FF FF",
+                      "30 00 00 00 02 01 FF FF 00 00 00 00 00 00 00 00",
+                      "FF FF FF FF FF FF FF FF 00 00 00 00 2E 00 43 00",
+                      "1D 00 00 00 25 00 00 00 FF FF FF FF 40 00 00 00")
+    cache.indirect_table = bytes.fromhex(" ".join(indirect_table))
+    object_table = ("02 00 53 4C FF FF FF FF 00 00 01 00 53 10 FF FF",
+                    "FF FF 00 00 01 00 53 94 FF FF FF FF 00 00 00 00",
+                    "02 3C FF FF FF FF 00 00")
+    cache.object_table = bytes.fromhex(" ".join(object_table))
+    cache.pcode = b''
+
     module = DocModule("foo")
     path1 = "vbaProjectCompiler/blank_files/ThisWorkbook.cls"
     os.remove(path1 + ".new")
@@ -20,7 +40,7 @@ def test_normalize():
     # while line := f.readline():
     #     assert line == e.readline()
 
-    module.create_cache()
+    module.set_cache(cache.to_bytes())
     module.write_file()
     path3 = path1 + ".bin"
     f_stream = open(path3, "rb")
