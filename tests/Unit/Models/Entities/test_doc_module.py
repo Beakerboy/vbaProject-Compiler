f3651c20e1b5be3526e814e0a1bf86023bf79b4d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-28 15:40:49 -0500

    Update test_doc_module.py

--- tests/Unit/Models/Entities/test_doc_module.py
+++ tests/Unit/Models/Entities/test_doc_module.py
@@ -4,33 +4,6 @@ from ms_ovba_compression.ms_ovba import MsOvba
 from vbaProjectCompiler.Models.Entities.doc_module import DocModule
 
 
-def test_create_cache():
-    this_workbook = DocModule("ThisWorkbook")
-    assert this_workbook.get_name() == "ThisWorkbook"
-    this_workbook.cookie.value = 0xB81C
-    guid = uuid.UUID('0002081900000000C000000000000046')
-    this_workbook.set_guid(guid)
-    this_workbook.create_cache()
-
-    f = open('tests/blank/vbaProject.bin', 'rb')
-    f.seek(0x0800)
-    file_data = f.read(0x0333)
-    assert this_workbook.get_cache() == file_data
-
-
-def test_create_cache2():
-    module = DocModule("Sheet1")
-    module.cookie.value = 0x9B9A
-    guid = uuid.UUID('0002082000000000C000000000000046')
-    module.set_guid(guid)
-    module.create_cache()
-
-    f = open('tests/blank/vbaProject.bin', 'rb')
-    f.seek(0x0C00)
-    file_data = f.read(0x0333)
-    assert module.get_cache() == file_data
-
-
 def test_normalize():
     module = DocModule("foo")
     path1 = "vbaProjectCompiler/blank_files/ThisWorkbook.cls"
