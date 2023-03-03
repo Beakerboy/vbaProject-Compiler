f1a79ed4696a0e77d32e1fff1b8f88fb89a573ed
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-03 17:10:01 -0500

    Update projectWm.py

--- vbaProjectCompiler/Views/projectWm.py
+++ vbaProjectCompiler/Views/projectWm.py
@@ -5,7 +5,7 @@ class ProjectWm:
     def __init__(self, project):
         self.project = project
 
-    def toBytes(self):
+    def to_bytes(self):
         output = b''
         for module in self.project.modules:
             output += (bytes(module.modName.value, 'ascii')
@@ -14,3 +14,9 @@ class ProjectWm:
                        + b'\x00\x00')
         output += b'\x00\x00'
         return output
+
+    def write_file(self):
+        bin_f = open("project.bin", "wb")
+        ms_ovba = MsOvba()
+        bin_f.write(self.to_bytes())
+        bin_f.close()
