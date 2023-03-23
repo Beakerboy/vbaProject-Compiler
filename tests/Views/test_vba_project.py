8f3bdd3b8446dd944642587e63c06ec2d2ada9d1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-23 15:44:22 -0400

    Update test_vba_project.py

--- tests/Views/test_vba_project.py
+++ tests/Views/test_vba_project.py
@@ -1,10 +1,10 @@
 from vbaProjectCompiler.vbaProject import VbaProject
-from vbaProjectCompiler.Views.vba_Project import Vba_Project
+from vbaProjectCompiler.Views.project_view import ProjectView
 
 
 def test_vba_project() -> None:
     vba_project = VbaProject()
-    vba_project_view = Vba_Project(vba_project)
+    vba_project_view = ProjectView(vba_project)
     expected = b'\xCC\x61\xFF\xFF\x00\x03\x00'
     assert vba_project_view.to_bytes() == expected
     vba_project.setPerformanceCache(b'\x00\x01\x02\x03')
