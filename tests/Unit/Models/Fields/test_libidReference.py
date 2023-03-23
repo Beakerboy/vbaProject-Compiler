dc859ec422fa1498dd337bf1f65426f4cdfb1394
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-23 13:19:59 -0400

    Update test_libidReference.py

--- tests/Unit/Models/Fields/test_libidReference.py
+++ tests/Unit/Models/Fields/test_libidReference.py
@@ -13,13 +13,13 @@ def test_str():
     )
     expected = ("*\\G{00020430-0000-0000-C000-000000000046}"
                 "#2.0#0#C:\\Windows\\System32\\stdole2.tlb#OLE Automation")
-    assert str(libidRef) == expected
-    assert len(libidRef) == 94
+    assert str(libid_ref) == expected
+    assert len(libid_ref) == 94
 
 
 def test_posix():
     guid = uuid.UUID('0002043000000000C000000000000046')
-    libidRef = LibidReference(
+    libid_ref = LibidReference(
         guid,
         "2.0",
         "0",
