2a30f1ef91f2f7e0ebdbddb5053dc737c330e899
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-23 11:16:59 -0400

    Update test_dirObject.py

--- tests/Functional/test_dirObject.py
+++ tests/Functional/test_dirObject.py
@@ -10,7 +10,7 @@ from vbaProjectCompiler.Models.Entities.std_module import StdModule
 from vbaProjectCompiler.Models.Entities.referenceRecord import ReferenceRecord
 
 
-def test_dirstream():
+def test_dirstream() -> None:
     module_cache = ModuleCache(0xB5, 0x08F3)
     # Read the data from the demo file and decompress it.
     f = open('tests/blank/vbaProject.bin', 'rb')
@@ -19,13 +19,13 @@ def test_dirstream():
     f.seek(offset)
     container = f.read(length)
     ms_ovba = MsOvba()
-    decompressedStream = ms_ovba.decompress(container)
+    decompressed_stream = ms_ovba.decompress(container)
 
     # Create a project with the same attributes
     project = VbaProject()
     stream = DirStream(project)
-    codePage = 0x04E4
-    codePageName = "cp" + str(codePage)
+    codepage = 0x04E4
+    codepage_name = "cp" + str(codepage)
     guid = uuid.UUID('0002043000000000C000000000000046')
     libidRef = LibidReference(
         guid,
@@ -34,18 +34,18 @@ def test_dirstream():
         "C:\\Windows\\System32\\stdole2.tlb",
         "OLE Automation"
     )
-    oleReference = ReferenceRecord(codePageName, "stdole", libidRef)
+    ole_reference = ReferenceRecord(codepage_name, "stdole", libidRef)
     guid = uuid.UUID('2DF8D04C5BFA101BBDE500AA0044DE52')
-    libidRef2 = LibidReference(
+    libid_ref2 = LibidReference(
         guid,
         "2.0",
         "0",
         "C:\\Program Files\\Common Files\\Microsoft Shared\\OFFICE16\\MSO.DLL",
         "Microsoft Office 16.0 Object Library"
     )
-    officeReference = ReferenceRecord(codePageName, "Office", libidRef2)
-    project.addReference(oleReference)
-    project.addReference(officeReference)
+    office_reference = ReferenceRecord(codepage_name, "Office", libid_ref2)
+    project.addReference(ole_reference)
+    project.addReference(office_reference)
     project.setProjectCookie(0x08F3)
 
     indirect_table = ("02 80 FE FF FF FF FF FF 20 00 00 00 FF FF FF FF",
@@ -89,8 +89,8 @@ def test_dirstream():
     project.addModule(sheet1)
     project.addModule(module1)
 
-    assert stream.to_bytes() == decompressedStream
+    assert stream.to_bytes() == decompressed_stream
 
     # The compression results are not the same.
     compressed = ms_ovba.compress(stream.to_bytes())
-    assert ms_ovba.decompress(compressed) == decompressedStream
+    assert ms_ovba.decompress(compressed) == decompressed_stream
