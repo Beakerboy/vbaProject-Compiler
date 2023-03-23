6bc655671359355ce2f536000dfd4c11abfbac91
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-23 13:25:27 -0400

    Update test_projectWm.py

--- tests/Views/test_projectWm.py
+++ tests/Views/test_projectWm.py
@@ -5,18 +5,18 @@ from vbaProjectCompiler.Views.projectWm import ProjectWm
 
 
 def test_projectWm():
-    vbaProject = VbaProject()
-    projectWm = ProjectWm(vbaProject)
+    vba_project = VbaProject()
+    project_wm = ProjectWm(vba_project)
     thisWorkbook = DocModule("ThisWorkbook")
     sheet1 = DocModule("Sheet1")
     module1 = StdModule("Module1")
-    vbaProject.addModule(thisWorkbook)
+    vbaProject.addModule(this_workbook)
     vbaProject.addModule(sheet1)
     vbaProject.addModule(module1)
     expected = (b'ThisWorkbook\x00T\x00h\x00i\x00s\x00W\x00o\x00r\x00k\x00b'
                 + b'\x00o\x00o\x00k\x00\x00\x00Sheet1\x00S\x00h\x00e\x00e\x00'
                 + b't\x001\x00\x00\x00Module1\x00M\x00o\x00d\x00u\x00l\x00e'
                 + b'\x001\x00\x00\x00\x00\x00')
-    result = projectWm.to_bytes()
+    result = project_wm.to_bytes()
     assert len(result) == 86
     assert result == expected
