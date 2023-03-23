2a816623a43d1577a9fe873ef9086c2562b764db
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-23 15:13:30 -0400

    Update packedData.py

--- vbaProjectCompiler/Models/Fields/packedData.py
+++ vbaProjectCompiler/Models/Fields/packedData.py
@@ -5,10 +5,10 @@ class PackedData():
     """
     Mutivalue field with a packing format
     """
-    def __init__(self, format, *values):
+    def __init__(self, format, *values) -> None:
         self.values = values
         self.format = format
 
-    def pack(self, codePageName, endien):
-        endienSymbol = '<' if endien == 'little' else '>'
-        return struct.pack(endienSymbol + self.format, *self.values)
+    def pack(self, codepage_name, endien) -> bytes:
+        endien_symbol = '<' if endien == 'little' else '>'
+        return struct.pack(endien_symbol + self.format, *self.values)
