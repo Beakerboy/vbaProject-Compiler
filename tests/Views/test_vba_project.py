56932cecbfec274d54b309d239faa6a2d558d458
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-30 13:09:36 -0400

    Update test_vba_project.py

--- tests/Views/test_vba_project.py
+++ tests/Views/test_vba_project.py
@@ -7,7 +7,7 @@ def test_vba_project() -> None:
     vba_project_view = ProjectView(vba_project)
     expected = b'\xCC\x61\xFF\xFF\x00\x03\x00'
     assert vba_project_view.to_bytes() == expected
-    vba_project.setPerformanceCache(b'\x00\x01\x02\x03')
-    vba_project.setPerformanceCacheVersion(0x00B5)
+    vba_project.set_performance_cache(b'\x00\x01\x02\x03')
+    vba_project.set_performance_cache_version(0x00B5)
     expected = b'\xCC\x61\xB5\x00\x00\x03\x00\x00\x01\x02\x03'
     assert vba_project_view.to_bytes() == expected
