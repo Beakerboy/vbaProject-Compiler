72bc9cf7a7b78de35616bd4fc742a4bdc3294428
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-30 11:59:29 -0400

    Update project_view.py

--- vbaProjectCompiler/Views/project_view.py
+++ vbaProjectCompiler/Views/project_view.py
@@ -15,11 +15,11 @@ class ProjectView:
         reserved1 = 0x61CC
         reserved2 = 0x00
         reserved3 = 0x0003
-        cache_version = self.project.getPerformanceCacheVersion()
+        cache_version = self.project.get_performance_cache_version()
 
         output += struct.pack(format, reserved1, cache_version,
                               reserved2, reserved3)
-        return output + self.project.getPerformanceCache()
+        return output + self.project.get_performance_cache()
 
     def write_file(self) -> None:
         bin_f = open("vba_project.bin", "wb")
