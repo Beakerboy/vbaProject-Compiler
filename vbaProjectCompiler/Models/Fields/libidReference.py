cbfbe2fa51fca46535de3bf2961506efd6fd8792
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-30 15:27:48 -0400

    Update libidReference.py

--- vbaProjectCompiler/Models/Fields/libidReference.py
+++ vbaProjectCompiler/Models/Fields/libidReference.py
@@ -1,24 +1,24 @@
 class LibidReference():
-    def __init__(self, libidGuid, version,
-                 libidLcid, libidPath, libidRegName):
-        self.libidGuid = libidGuid
-        self.version = version
-        self.libidLcid = libidLcid
-        self.libidPath = libidPath
-        self.libidRegName = libidRegName
-        if self._is_windows_path(libidPath):
-            self.libidReferenceKind = "G"
+    def __init__(self, libid_guid, version,
+                 libid_lcid, libid_path, libid_reg_name):
+        self._libid_guid = libid_guid
+        self._version = version
+        self._libid_lcid = libid_lcid
+        self._libid_path = libid_path
+        self._libid_reg_name = libid_reg_name
+        if self._is_windows_path(libid_path):
+            self._libid_reference_kind = "G"
         else:
-            self.libidReferenceKind = "H"
+            self._libid_reference_kind = "H"
 
     def __str__(self):
         return "*\\" + \
-            self.libidReferenceKind + \
-            "{" + str(self.libidGuid).upper() + "}#" + \
-            self.version + "#" + \
-            self.libidLcid + "#" + \
-            str(self.libidPath) + "#" + \
-            self.libidRegName
+            self._libid_reference_kind + \
+            "{" + str(self._libid_guid).upper() + "}#" + \
+            self._version + "#" + \
+            self._libid_lcid + "#" + \
+            str(self._libid_path) + "#" + \
+            self._libid_reg_name
 
     def __len__(self):
         return len(str(self))
