c473ef37a9325486b8b7153aedaee877934eca77
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-01 18:11:15 -0400

    Update project_view.py

--- vbaProjectCompiler/Views/project_view.py
+++ vbaProjectCompiler/Views/project_view.py
@@ -1,11 +1,15 @@
 import struct
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='ProjectView')
 
 
 class ProjectView:
     """
     The _VBA_PROJECT data view for the vbaProject
     """
-    def __init__(self, project) -> None:
+    def __init__(self: T, project) -> None:
         self.project = project
 
     def to_bytes(self) -> bytes:
@@ -21,7 +25,7 @@ class ProjectView:
                               reserved2, reserved3)
         return output + self.project.get_performance_cache()
 
-    def write_file(self) -> None:
+    def write_file(self: T) -> None:
         bin_f = open("vba_project.bin", "wb")
         bin_f.write(self.to_bytes())
         bin_f.close()
