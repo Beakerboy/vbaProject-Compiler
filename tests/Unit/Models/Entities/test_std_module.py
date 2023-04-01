271cb4d907ee47a51998d29fbc94b436d90dd38c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-01 11:59:55 -0400

    Update test_std_module.py

--- tests/Unit/Models/Entities/test_std_module.py
+++ tests/Unit/Models/Entities/test_std_module.py
@@ -1,13 +1,13 @@
 from vbaProjectCompiler.Models.Entities.std_module import StdModule
 
 
-def test_set_get_cache():
+def test_set_get_cache() -> None:
     module = StdModule("Module1")
     cache = b'foo'
     module.set_cache(cache)
     assert module.get_cache() == cache
 
 
-def test_get_name():
+def test_get_name() -> None:
     module = StdModule("Module1")
     assert module.get_name() == "Module1"
