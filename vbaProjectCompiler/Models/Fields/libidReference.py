e7597ac6b0c41619f6770b2ec9eb112dd271c886
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-02 11:17:19 -0400

    Update libidReference.py

--- vbaProjectCompiler/Models/Fields/libidReference.py
+++ vbaProjectCompiler/Models/Fields/libidReference.py
@@ -1,6 +1,13 @@
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='LibidReference')
+
+
 class LibidReference():
-    def __init__(self, libid_guid, version,
-                 libid_lcid, libid_path, libid_reg_name):
+    def __init__(self: T, libid_guid, version: str,
+                 libid_lcid: str, libid_path: str,
+                 libid_reg_name: str) -> None:
         self._libid_guid = libid_guid
         self._version = version
         self._libid_lcid = libid_lcid
@@ -11,7 +18,7 @@ class LibidReference():
         else:
             self._libid_reference_kind = "H"
 
-    def __str__(self):
+    def __str__(self: T) -> str:
         return "*\\" + \
             self._libid_reference_kind + \
             "{" + str(self._libid_guid).upper() + "}#" + \
@@ -20,8 +27,8 @@ class LibidReference():
             str(self._libid_path) + "#" + \
             self._libid_reg_name
 
-    def __len__(self):
+    def __len__(self: T) -> int:
         return len(str(self))
 
-    def _is_windows_path(self, path):
+    def _is_windows_path(self: T, path: str) -> bool:
         return path[0] != '/'
