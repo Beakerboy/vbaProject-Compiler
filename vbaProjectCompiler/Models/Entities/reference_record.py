782612aa18d601635786d60314de605995c338ea
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-03 08:42:47 -0400

    Update reference_record.py

--- vbaProjectCompiler/Models/Entities/reference_record.py
+++ vbaProjectCompiler/Models/Entities/reference_record.py
@@ -1,6 +1,7 @@
 from vbaProjectCompiler.Models.Fields.doubleEncodedString import (
     DoubleEncodedString
 )
+from vbaProjectCompiler.Models.Fields.libid_reference import LibidReference
 from vbaProjectCompiler.Models.Fields.packed_data import PackedData
 from typing import TypeVar
 
@@ -10,13 +11,13 @@ T = TypeVar('T', bound='ReferenceRecord')
 
 class ReferenceRecord():
 
-    def __init__(self: T, codepage_name: str, name: str, libid_ref):
+    def __init__(self: T, codepage_name: str, name: str, libid_ref: LibidReference) -> None:
         # is self._codepage_name even needed?
         self._codepage_name = codepage_name
         self._refname = DoubleEncodedString([0x0016, 0x003E], name)
         self._libid_ref = libid_ref
 
-    def pack(self: T, codepage_name, endien):
+    def pack(self: T, codepage_name: str, endien: str) -> bytes:
         strlen = len(self._libid_ref)
         format = "HII" + str(strlen) + "sIH"
         lib_str = str(self._libid_ref).encode(self._codepage_name)
