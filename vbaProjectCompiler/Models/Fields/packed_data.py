7771929a591f9bf3df6bbbcc065f00cace2af4b3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-02 22:37:00 -0400

    Update and rename packedData.py to packed_data.py

--- vbaProjectCompiler/Models/Fields/packed_data.py
+++ vbaProjectCompiler/Models/Fields/packed_data.py
@@ -1,14 +1,18 @@
 import struct
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='PackedData')
 
 
 class PackedData():
     """
     Mutivalue field with a packing format
     """
-    def __init__(self, format, *values) -> None:
+    def __init__(self:T, format: str, *values) -> None:
         self.values = values
         self.format = format
 
-    def pack(self, codepage_name, endien) -> bytes:
+    def pack(self: T, codepage_name: str, endien: str) -> bytes:
         endien_symbol = '<' if endien == 'little' else '>'
         return struct.pack(endien_symbol + self.format, *self.values)
