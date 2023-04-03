6663656134be46f26bc6040f954ad70e0c8ac2f3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-03 08:08:24 -0400

    Update std_module.py

--- vbaProjectCompiler/Models/Entities/std_module.py
+++ vbaProjectCompiler/Models/Entities/std_module.py
@@ -1,13 +1,17 @@
 from vbaProjectCompiler.Models.Entities.module_base import ModuleBase
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='StdModule')
 
 
 class StdModule(ModuleBase):
 
-    def __init__(self, name):
+    def __init__(self: T, name: str) -> None:
         super(StdModule, self).__init__(name)
         self.type = "Module"
 
-    def normalize_file(self):
+    def normalize_file(self: T) -> None:
         f = open(self._file_path, "r")
         new_f = open(self._file_path + ".new", "a+", newline='\r\n')
         while line := f.readline():
