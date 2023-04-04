eadcdd8221a68ebab19f5b4eb7f7a91518953358
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-04 07:51:25 -0400

    Update test_fullFile.py

--- tests/Functional/test_fullFile.py
+++ tests/Functional/test_fullFile.py
@@ -5,12 +5,12 @@ import unittest.mock
 import uuid
 from ms_ovba_compression.ms_ovba import MsOvba
 from ms_pcode_assembler.module_cache import ModuleCache
-from vbaProjectCompiler.vbaProject import VbaProject
-from vbaProjectCompiler.Models.Entities.doc_module import DocModule
-from vbaProjectCompiler.Models.Entities.std_module import StdModule
-from vbaProjectCompiler.Models.Entities.reference_record import ReferenceRecord
-from vbaProjectCompiler.Models.Fields.libid_reference import LibidReference
-from vbaProjectCompiler.Views.project_ole_file import ProjectOleFile
+from vbaproject_compiler.vbaProject import VbaProject
+from vbaproject_compiler.Models.Entities.doc_module import DocModule
+from vbaproject_compiler.Models.Entities.std_module import StdModule
+from vbaproject_compiler.Models.Entities.reference_record import ReferenceRecord
+from vbaproject_compiler.Models.Fields.libid_reference import LibidReference
+from vbaproject_compiler.Views.project_ole_file import ProjectOleFile
 from typing import Type, TypeVar
 
 
