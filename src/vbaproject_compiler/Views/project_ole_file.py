b05fdfd510d1732dfd64dda71b7524bc3a5ee25b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-05-02 11:17:55 -0400

    Update project_ole_file.py

--- src/vbaproject_compiler/Views/project_ole_file.py
+++ src/vbaproject_compiler/Views/project_ole_file.py
@@ -18,36 +18,43 @@ class ProjectOleFile:
     def __init__(self: T, project: VbaProject) -> None:
         self._project = project
 
-    def _create_binary_files(self: T) -> None:
+    def _build_ole_directory(self: T) -> RootDirectory:
         """
         Create all the custom views for the OLE file:
             dir
             project
             projectWm
             vbs_project
-        ToDo
-            SLP streams
-        """
-        for module in self._project.get_modules():
-            module.write_file()
-        dir = DirStream(self._project)
-        dir.write_file()
-        project = Project(self._project)
-        project.write_file()
-        project_wm = ProjectWm(self._project)
-        project_wm.write_file()
-        project_view = ProjectView(self._project)
-        project_view.write_file()
 
-    def _build_ole_directory(self: T) -> RootDirectory:
-        """
         Organize the modules and views into the correct storage directories
         """
         directory = RootDirectory()
         storage = StorageDirectory("VBA")
         for module in self._project.get_modules():
+            module.write_file()
             dir = StreamDirectory(module.get_name(), module.get_bin_path())
             storage.add_directory(dir)
+
+        module = DirStream(self._project)
+        module.write_file()
+        dir = StreamDirectory(module.get_name(), module.get_bin_path())
+        storage.add_directory(dir)
+
+        module = Project(self._project)
+        module.write_file()
+        dir = StreamDirectory(module.get_name(), module.get_bin_path())
+        storage.add_directory(dir)
+
+        module = ProjectWm(self._project)
+        module.write_file()
+        dir = StreamDirectory(module.get_name(), module.get_bin_path())
+        storage.add_directory(dir)
+
+        module = ProjectView(self._project)
+        module.write_file()
+        dir = StreamDirectory(module.get_name(), module.get_bin_path())
+        storage.add_directory(dir)
+
         directory.add_directory(storage)
         stream = StreamDirectory("PROJECTwm", "projectwm.bin")
         directory.add_directory(stream)
@@ -61,6 +68,5 @@ class ProjectOleFile:
         ole_file.create_file("vbaProject.bin")
 
     def write_file(self: T) -> None:
-        self._create_binary_files()
         directory = self._build_ole_directory()
         self._write_ole_file(directory)
