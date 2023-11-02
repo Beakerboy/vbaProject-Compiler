827f0f3bc0e333e8a2ef2bf957bcccdb0b509234
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-02 13:06:17 -0400

    Update __main__.py

--- src/vbaproject_compiler/__main__.py
+++ src/vbaproject_compiler/__main__.py
@@ -1,4 +1,7 @@
 import argparse
+import glob
+from vbaproject_compiler.vbaProject import VbaProject
+from vbaproject_compiler.Views.project_ole_file import ProjectOleFile
 
 def main(args: list) -> None:
     parser = argparse.ArgumentParser()
@@ -9,7 +12,29 @@ def main(args: list) -> None:
     args = parser.parse_args()
     # cd args.output
     # build a list of all bas, cls, frm, and frx files
+    bas_files = glob.glob('*.bas')
+    cls_files = glob.glob('*.cls')
+    frm_files = glob.glob('*.frm')
+    frx_files = glob.glob('*.frx')
+
     # create a new project object
+    project = VbaProject()
+
+    # add default modules
+    module = DocModule('Sheet1')
+    module.addFile('src/vbaproject_compiler/blank_files/Sheet1.cls')
+    project.addModule(module)
+    module = DocModule('ThisWorkbook')
+    module.addFile('src/vbaproject_compiler/blank_files/ThisWorkbook.cls')
+    project.addModule(module)
+    
     # add the files
-    # create the output directory
-    # save the deliverables.
+    foreach bas_files as file:
+        #name = get name from file
+        code = StdModule(name)
+        code.addFile(file)
+        project.addModule(code)
+    
+    ole_file = ProjectOleFile(project)
+    ole_file.write_file()
+    
