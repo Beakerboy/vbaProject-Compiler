b5eeec355c3dee86fc86d9a98afbc736181454a1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-02 11:47:53 -0400

    Update __main__.py

--- src/vbaproject_compiler/__main__.py
+++ src/vbaproject_compiler/__main__.py
@@ -1,2 +1,15 @@
+import argparse
+
 def main(args: list) -> None:
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory",
+                        help="The directory that contains your files.")
+    parser.add_argument("-o", "--output",
+                        help="The output file name.")
+    args = parser.parse_args()
+    # cd args.output
+    # build a list of all bas, cls, frm, and frx files
+    # create a new project object
+    # add the files
+    # create the output directory
+    # save the deliverables.
